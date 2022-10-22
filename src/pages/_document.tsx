2ab6c755270fbd02d19ec29e354764b05142eb08
Jean Paul Tivan <jptivan53@gmail.com>
2022-10-22 18:55:04 -0400

    Estilos para SSR

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { getCssText } from "@config/stitches.config";
 
 export default function Document() {
   return (
@@ -7,6 +8,10 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?family=Inter&family=Montserrat:wght@400;500;600;700;800;900&display=swap"
           rel="stylesheet"
+        />
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
         />
       </Head>
       <body>
