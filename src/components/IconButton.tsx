61b7ad0cb1e11dd15dc7bba266987c28128e70d9
Jean Paul Tivan <jptivan53@gmail.com>
2022-10-22 02:32:46 -0400

    Boton que contiene un icono

---
+++ src/components/IconButton.tsx
@@ -0,0 +1,25 @@
+import { styled } from "@config/stitches.config";
+
+const IconButton = styled("button", {
+  all: "unset",
+  display: "inline-flex",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: 6,
+  border: "1px solid $sand5",
+  color: "$orange9",
+  borderRadius: 6,
+  cursor: "pointer",
+
+  "&:hover": {
+    borderColor: "$orange9",
+  },
+
+  "& svg": {
+    width: 20,
+    height: 20,
+    fill: "currentColor",
+  },
+});
+
+export default IconButton;
