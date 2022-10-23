10a65c1a65eb4f1a9c790397a5d74c5d9dafe679
Jean Paul Tivan <jptivan53@gmail.com>
2022-10-23 19:44:36 -0400

    NEW: Container fluido se ajusta al tamaño de los bp

---
+++ src/components/Container.tsx
@@ -0,0 +1,25 @@
+import { styled } from "@config/stitches.config";
+
+const Container = styled("div", {
+  marginTop: 0,
+  marginBottom: 0,
+  marginLeft: "auto",
+  marginRight: "auto",
+  paddingLeft: 24,
+  paddingRight: 24,
+  paddingTop: 32,
+  paddingBottom: 32,
+  zIndex: 1,
+
+  width: "100%",
+  "@md": {
+    paddingLeft: 32,
+    paddingRight: 32,
+    maxWidth: 768,
+  },
+  "@lg": {
+    maxWidth: 1200,
+  },
+});
+
+export default Container;
