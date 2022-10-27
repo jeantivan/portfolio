7c88cb598e020871b9fa495343302c218d625d06
Jean Paul Tivan <jptivan53@gmail.com>
2022-10-27 02:42:41 -0400

    ADD: Pasa los valores de las cookies de ajustes como props iniciales de la app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
-import type { AppProps } from "next/app";
+import App, { AppContext, AppInitialProps, AppProps } from "next/app";
+import { getCookie, setCookie, hasCookie } from "cookies-next";
 import { globalCss } from "@config/stitches.config";
 import Layout from "@components/Layout";
-import SettingsProvider from "@components/Settings";
+import Settings from "@components/Settings";
+import { TColorMode, TColors } from "@src/types";
+import { COLOR_MODE_KEY, PRIMARY_COLOR_KEY } from "@src/constants";
+
+type AppOwnProps = { color: TColors; mode: TColorMode };
 
 const globalStyles = globalCss({
   "html, body": {
@@ -9,21 +14,39 @@ const globalStyles = globalCss({
     margin: "0",
     fontFamily:
       "-apple-system, BlinkMacSystemFont, Inter, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
-    backgroundColor: "$bg",
-    color: "$orange1",
   },
   "*": { boxSizing: "border-box", overflowX: "hidden" },
 });
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, color, mode }: AppProps & AppOwnProps) {
   globalStyles();
   return (
-    <SettingsProvider>
+    <Settings color={color} mode={mode}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </SettingsProvider>
+    </Settings>
   );
 }
+
+MyApp.getInitialProps = async (
+  context: AppContext
+): Promise<AppOwnProps & AppInitialProps> => {
+  const ctx = await App.getInitialProps(context);
+  const { req, res } = context.ctx;
+
+  if (!hasCookie(PRIMARY_COLOR_KEY, { req, res })) {
+    setCookie(PRIMARY_COLOR_KEY, "orange", { req, res });
+  }
+  if (!hasCookie(COLOR_MODE_KEY, { req, res })) {
+    setCookie(COLOR_MODE_KEY, "dark", { req, res });
+  }
+
+  return {
+    ...ctx,
+    color: (getCookie(PRIMARY_COLOR_KEY, { req, res }) || "orange") as TColors,
+    mode: (getCookie(COLOR_MODE_KEY, { req, res }) || "dark") as TColorMode,
+  };
+};
 
 export default MyApp;
