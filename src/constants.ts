d956fb973fef00ff62470ba16e77ffda05114e6e
Jean Paul Tivan <jptivan53@gmail.com>
2022-10-27 02:39:12 -0400

    NEW: Constantes

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const PRIMARY_COLOR_KEY = "jeantivan-portfolio-primary-color";
+export const COLOR_MODE_KEY = "jeantivan-portfolio-color-mode";
+export const COLORS = ["orange", "blue", "grass", "amber", "purple"] as const;
