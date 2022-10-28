35266475ae57b57fd99a35a37505106debc55b2a
Jean Paul Tivan <jptivan53@gmail.com>
2022-10-27 23:41:04 -0400

    CHANGE: maxWidth en @lg es 960. Eliminado los margenes y padding superior e inferior

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,14 +1,10 @@
 import { styled } from "@config/stitches.config";
 
 const Container = styled("div", {
-  marginTop: 0,
-  marginBottom: 0,
   marginLeft: "auto",
   marginRight: "auto",
   paddingLeft: 24,
   paddingRight: 24,
-  paddingTop: 32,
-  paddingBottom: 32,
   //zIndex: 1,
 
   width: "100%",
@@ -18,7 +14,7 @@ const Container = styled("div", {
     maxWidth: 768,
   },
   "@lg": {
-    maxWidth: 1200,
+    maxWidth: 960,
   },
 });
 
