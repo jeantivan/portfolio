c18cb8a592a88d97bf2c2e99701073fd56968c95
Jean Paul Tivan <jptivan53@gmail.com>
2022-10-31 19:04:45 -0400

    ADD: Estilos para convertir el comp en una card

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,5 +1,14 @@
 import NextImage from "next/image";
-import { styled } from "@config/stitches.config";
+import { styled, keyframes } from "@config/stitches.config";
+
+const spin = keyframes({
+  "0%": {
+    transform: "rotate(0deg)",
+  },
+  "100%": {
+    transform: "rotate(360deg)",
+  },
+});
 
 const Image = styled(NextImage, {});
 
@@ -7,51 +16,55 @@ const Name = styled("span", {
   fontSize: 18,
   marginTop: 12,
   textAlign: "center",
+  color: "$text",
+  fontFamily: "$mont",
+  fontWeight: "500",
 });
 
 const Container = styled("div", {
-  minHeight: 72,
-  padding: 12,
+  borderRadius: 6,
+  backgroundColor: "$menu",
+  boxShadow: "0px 2px 6px rgba(0,0,0,0.1)",
+  width: "100%",
+  height: "100%",
+  zIndex: 1,
+  transition: "all 150ms ease-in",
+  padding: 10,
   display: "flex",
   flexDirection: "column",
-  justifyContent: "center",
+  justifyContent: "space-between",
   alignItems: "center",
-  border: "1px solid $sand3",
-  borderRadius: 6,
-  color: "$text",
 
   "&:hover": {
-    borderColor: "$primary",
+    boxShadow: "0px 0px 16px $colors$primary",
   },
 
   variants: {
-    list: {
+    rotate: {
       true: {
-        flexDirection: "row",
-        justifyContent: "flex-start",
-
-        [`& ${Name}`]: {
-          marginLeft: 32,
-          marginTop: 0,
+        "& img": {
+          animation: `${spin} infinite 20s linear`,
         },
       },
     },
   },
 });
 
-const Skill = ({
-  name,
-  image,
-  list,
-}: {
-  name: string;
-  image: string;
-  list: boolean;
-}) => {
+const Skill = ({ name, image }: { name: string; image: string }) => {
+  const isReact = name === "React";
+
   return (
-    <Container list={list}>
-      <Image alt={`${name} logo`} src={image} width={48} height={48} />
-      <Name>{name}</Name>
+    <Container
+      css={{ gridArea: name.toLowerCase().replace(" ", "-") }}
+      rotate={isReact}
+    >
+      <Image
+        alt={`${name} logo`}
+        src={image}
+        height={isReact ? 80 : 40}
+        width={isReact ? 80 : 40}
+      />
+      {isReact && <Name>React.js</Name>}
     </Container>
   );
 };
