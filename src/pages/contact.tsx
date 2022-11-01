46325087c291d23c5bef6568368378460700acdb
Jean Paul Tivan <jptivan53@gmail.com>
2022-10-31 20:22:02 -0400

    ADD: Pagina de contacto

---
+++ src/pages/contact.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Container from "@components/Container";
+
+const Contact: NextPage = () => {
+  return (
+    <Container>
+      <Head>
+        <title>Contact | JeanTivan</title>
+      </Head>
+      <h1>Contact page</h1>
+    </Container>
+  );
+};
+
+export default Contact;
