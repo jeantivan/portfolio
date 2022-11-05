535fdd3bad5572b2641a8e2ab4b6a7d09589bb16
Jean Paul Tivan <jptivan53@gmail.com>
2022-11-04 20:59:19 -0400

    CHANGE: Layout y contenido dummy de la pagina

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,129 +1,46 @@
-import type { NextPage } from "next";
 import Head from "next/head";
+import type { NextPage } from "next";
 import { styled } from "@config/stitches.config";
 import Container from "@components/Container";
-import IconButton from "@src/components/IconButton";
-import { ExternalLinkIcon } from "@radix-ui/react-icons";
+import Project from "@components/Project";
+
+const Span = styled("span", {});
 
 const Grid = styled("div", {
-  width: "100%",
-  height: "100%",
   paddingTop: 32,
-  paddingBottom: 32,
   display: "grid",
   gridTemplateColumns: "repeat(6, 1fr)",
-  gridTemplateRows: "min-content min-content",
-  gridGap: "16px 16px",
-  alignItems: "start",
-});
-const Content = styled("div", {
-  gridColumn: "span 5",
-});
-const Title = styled("h1", {
-  fontFamily: "$mont",
-  fontWeight: 700,
-  fontSize: 72,
-  color: "$text",
-  margin: 0,
-  //marginBottom: 16,
-  paddingBottom: 12,
-  gridColumn: "span 5",
-  gridRow: "span 1",
+  gridGap: "16px",
 });
 
-const Span = styled("span", {
-  position: "relative",
-
-  "&:after": {
-    content: "''",
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    height: 6,
-    width: "50%",
-    backgroundColor: "$primary",
-    clipPath: "polygon(0 0, 100% 0%, 80% 100%, 0% 100%)",
-    transform: "translate(0, 12px)",
-  },
-});
-
-const Cards = styled("div", {
+const Title = styled("div", {
   gridColumn: "span 6",
-});
+  marginBottom: 24,
 
-const Card = styled("article", {
-  gridColumn: "span 6",
-  display: "flex",
-  flexDirection: "column",
-  height: "100%",
-
-  padding: 8,
-  borderRadius: 6,
-  border: "1px solid $sand7",
-
-  "@lg": {
-    gridColumn: "span 2",
+  "@md": {
+    gridColumn: "span 5",
   },
 });
 
-const CardImage = styled("div", {
-  width: "100%",
-  height: "0",
-  background: "#D9D9D9",
-  borderRadius: 3,
-  position: "relative",
-  paddingTop: "75%",
-  marginBottom: 10,
-
-  "& .placeholder": {
-    position: "absolute",
-    inset: 0,
-    width: "100%",
-    background: "#D9D9D9",
-    borderRadius: "inherit",
-  },
-});
-
-const CardContent = styled("div", {
-  display: "flex",
-  flexFlow: "row wrap",
-  marginBottom: 10,
-});
-
-const CartTitle = styled("h2", {
+const Text = styled("p", {
   margin: 0,
-  fontFamily: "$mont",
-  fontSize: 18,
-  fontWeight: 700,
   color: "$text",
-  flex: 1,
-  marginBottom: 8,
-});
+  fontSize: 16,
+  lineHeight: 1.25,
 
-const CardDescription = styled("p", {
-  margin: 0,
-  color: "$textSecondary",
-  fontSize: 14,
-});
-
-const CardFooter = styled("footer", {
-  marginTop: "auto",
-  display: "flex",
-});
-
-const Button = styled("button", {
-  all: "unset",
-  flex: 1,
-  display: "inline-flex",
-  justifyContent: "center",
-  alignItems: "center",
-  padding: 8,
-  background: "$primary",
-  borderRadius: 6,
-  marginRight: 8,
-
-  "&:hover": {
-    backgroundColor: "$primaryHover",
+  variants: {
+    heading: {
+      true: {
+        fontFamily: "$mont",
+        fontWeight: 700,
+        lineHeight: 1.25,
+      },
+    },
+    secondary: {
+      true: {
+        color: "$textSecondary",
+      },
+    },
   },
 });
 
@@ -135,61 +52,48 @@ const Projects: NextPage = () => {
       </Head>
       <Grid>
         <Title>
-          <Span>Projects</Span>
+          <Text
+            heading
+            as="h1"
+            css={{ fontSize: 44, marginBottom: 8, "@lg": { fontSize: 64 } }}
+          >
+            The <Span css={{ color: "$primary" }}>projects</Span> that{" "}
+            <Span css={{ color: "$primary" }}>I&apos;ve build</Span> so far.
+          </Text>
+          <Text secondary>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae,
+            consequuntur?
+          </Text>
         </Title>
-        <Card>
-          <CardImage>
-            <span className="placeholder" />
-          </CardImage>
-          <CardContent>
-            <CartTitle>Project title</CartTitle>
-            <CardDescription>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </CardDescription>
-          </CardContent>
-          <CardFooter>
-            <Button>Ver más</Button>
-            <IconButton>
-              <ExternalLinkIcon />
-            </IconButton>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardImage>
-            <span className="placeholder" />
-          </CardImage>
-          <CardContent>
-            <CartTitle>Project title</CartTitle>
-            <CardDescription>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
-              aspernatur beatae autem?
-            </CardDescription>
-          </CardContent>
-          <CardFooter>
-            <Button>Ver más</Button>
-            <IconButton>
-              <ExternalLinkIcon />
-            </IconButton>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardImage>
-            <span className="placeholder" />
-          </CardImage>
-          <CardContent>
-            <CartTitle>Project title</CartTitle>
-            <CardDescription>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. At,
-              sint!
-            </CardDescription>
-          </CardContent>
-          <CardFooter>
-            <Button>Ver más</Button>
-            <IconButton>
-              <ExternalLinkIcon />
-            </IconButton>
-          </CardFooter>
-        </Card>
+      </Grid>
+
+      <section>
+        <Project featured />
+        <Project featured />
+        <Project featured />
+      </section>
+      <Grid>
+        <Text
+          as="h3"
+          heading
+          css={{
+            fontSize: 24,
+            gridColumn: "span 5",
+          }}
+        >
+          <Span css={{ border: 0, borderBottom: "2px solid $primary" }}>
+            Small projects
+          </Span>
+        </Text>
+        <Text secondary css={{ gridColumn: "span 6" }}>
+          Interdum fermentum mollis leo eget vestibulum congue mauris, amet
+          ullamcorper. Dignissim molestie cras scelerisque augue et tellus
+          varius vitae.
+        </Text>
+        <Project />
+        <Project />
+        <Project />
+        <Project />
       </Grid>
     </Container>
   );
