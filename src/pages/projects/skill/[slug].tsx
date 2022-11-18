4e6ddc1eaba2c05bf0efc89246f0e7d6e57df86e
Jean Paul Tivan <jptivan53@gmail.com>
2022-11-17 20:22:03 -0400

    NEW: Mostrará los proyectos por skill

---
+++ src/pages/projects/skill/[slug].tsx
@@ -0,0 +1,55 @@
+import { NextPage, GetStaticProps, GetStaticPaths } from "next";
+import Head from "next/head";
+import { styled } from "@config/stitches.config";
+import Container from "@components/Container";
+import Text from "@components/Text";
+import { MAIN_SKILLS, PROJECT_EXAMPLE } from "@src/constants";
+import { TSkill, TProject } from "@src/types";
+
+const Span = styled("span", {});
+
+type PageProps = {
+  skill: TSkill;
+  projects: TProject[];
+};
+
+const SkillPage: NextPage<PageProps> = ({ skill, projects }) => {
+  return (
+    <Container css={{ paddingTop: 32, paddingBottom: 32 }}>
+      <Head>
+        <title>Projects with: {skill.name} | Jean Tivan</title>
+      </Head>
+      <Text heading css={{ fontSize: 40 }}>
+        Projects created using:{" "}
+        <Span css={{ color: "$primary" }}>{skill.name}</Span>
+      </Text>
+    </Container>
+  );
+};
+
+// TODO: Crear contenido en Sanity
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = MAIN_SKILLS.map((skill) => ({
+    params: {
+      slug: skill.slug,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false, // can also be true or 'blocking'
+  };
+};
+
+export const getStaticProps: GetStaticProps<PageProps> = async ({ params }) => {
+  const skill = MAIN_SKILLS.find((s) => s.slug === params?.slug);
+
+  return {
+    props: {
+      skill: skill || MAIN_SKILLS[0],
+      projects: [PROJECT_EXAMPLE],
+    },
+  };
+};
+
+export default SkillPage;
