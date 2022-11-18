551520f26434261e0cef45d02223040c0fd28a81
Jean Paul Tivan <jptivan53@gmail.com>
2022-11-17 20:22:35 -0400

    CHANGE: Diseño de la pagina

--- src/pages/project/example.tsx
+++ src/pages/project/example.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import {
   ArrowLeftIcon,
   ArrowRightIcon,
+  ChevronRightIcon,
   GitHubLogoIcon,
   GlobeIcon,
 } from "@radix-ui/react-icons";
@@ -13,25 +14,11 @@ import IconButton from "@src/components/IconButton";
 import Button from "@src/components/Button";
 import Flex from "@src/components/Flex";
 
-const Grid = styled("div", {
-  display: "grid",
-  gridTemplateColumns: "repeat(6, 1fr)",
-  gridTemplateRows: "repeat(4, min-content) 1fr",
-  gridTemplateAreas: `
-    "title title title title title title"
-    "display display display display links links"
-    "display display display display type type"
-    "display display display display skills skills"
-    "description description description description description ."
-  `,
-
-  "& > *": {
-    padding: 8,
-  },
-});
+const Span = styled("span", {});
+const Div = styled("div", {});
 
 const Display = styled("div", {
-  gridArea: "display",
+  marginBottom: 16,
   display: "grid",
   gridTemplateColumns: "repeat(5, 1fr)",
   gridTemplateRows: "1fr min-content",
@@ -40,9 +27,11 @@ const Display = styled("div", {
     "prev . dots . next";   
   `,
   gridGap: "12px 0",
-});
 
-const Div = styled("div", {});
+  "@lg": {
+    //width: "70%",
+  },
+});
 
 const Dot = styled("div", {
   width: 6,
@@ -65,39 +54,40 @@ const Dot = styled("div", {
   },
 });
 
-const ProjectType = styled("div", {
-  all: "unset",
-  display: "inline-block",
-  fontFamily: "$mont",
-  fontWeight: 700,
-  fontSize: 24,
-  color: "$text",
-  position: "relative",
-  overflowX: "hidden",
-  userSelect: "none",
-  cursor: "pointer",
-  transition: "all 150ms ease",
+const Skill = styled("div", {
+  fontSize: 12,
+  color: "$primary",
+  borderRadius: 999,
+  border: "1px solid $primary",
+  padding: "6px 20px",
+  display: "inline-flex",
+  alignItems: "center",
+  justifyContent: "center",
+  marginTop: 12,
 
-  "&:after": {
-    content: "''",
-    position: "absolute",
-    bottom: -4,
-    left: 0,
-    width: 100,
-    height: 4,
-    borderTopRightRadius: 8,
-    borderBottomRightRadius: 8,
-    background: "$primary",
-    transform: "translateX(-100%)",
-    willChange: "transform",
+  "&:not(:last-child)": { marginRight: 12 },
+});
+
+const Link = styled("a", {
+  all: "unset",
+  color: "$textSecondary",
+  cursor: "pointer",
+
+  "&:hover": {
+    textDecoration: "underline",
   },
 
-  "&:hover:after": {
-    transform: "translateX(0%)",
+  variants: {
+    active: {
+      true: {
+        color: "$text",
+      },
+    },
   },
 });
 
 const dots = [1, 2, 3, 4, 5];
+const skills = ["Javascript", "React", "Next", "Material UI"];
 
 const Project: NextPage = (props: any) => {
   return (
@@ -111,96 +101,132 @@ const Project: NextPage = (props: any) => {
       <Head>
         <title>Example Project | Jean Tivan</title>
       </Head>
-      <Grid>
-        <Div css={{ gridArea: "title" }}>
-          <Text as="h1" heading css={{ fontSize: 40, marginBottom: 8 }}>
-            Project title, it can be a large title
-          </Text>
-          <Text secondary css={{ fontSize: 14, fontStyle: "italic" }}>
-            Finished: 21/01/2022
-          </Text>
-        </Div>
-        <Display>
+      <Flex
+        css={{
+          marginBottom: 16,
+          alignItems: "center",
+          "& > *": {
+            marginRight: 8,
+          },
+          color: "$textSecondary",
+        }}
+      >
+        <Link>Projects</Link>
+        <Span css={{ display: "inline-flex" }}>
+          <ChevronRightIcon />
+        </Span>
+        <Link>Small projects</Link>
+        <Span css={{ display: "inline-flex" }}>
+          <ChevronRightIcon />
+        </Span>
+        <Link active>Project Name</Link>
+      </Flex>
+      <Div css={{ gridArea: "title", marginBottom: 16 }}>
+        <Text as="h1" heading css={{ fontSize: 32, "@md": { fontSize: 40 } }}>
+          Project title, it can be a large title
+        </Text>
+      </Div>
+      <Div
+        css={{
+          marginBottom: 16,
+        }}
+      >
+        <Flex>
+          <Button
+            css={{
+              maxWidth: 200,
+              flex: 1,
+              marginRight: 8,
+              "& svg": { marginRight: 12 },
+            }}
+          >
+            <GitHubLogoIcon /> Github
+          </Button>
+          <Button
+            css={{ maxWidth: 200, flex: 1, "& svg": { marginRight: 12 } }}
+          >
+            <GlobeIcon /> Preview
+          </Button>
+        </Flex>
+      </Div>
+      <Display>
+        <Div
+          css={{
+            gridArea: "gallery",
+            width: "100%",
+            position: "relative",
+            height: 0,
+            paddingTop: "61.78%",
+            overflow: "hidden",
+            borderRadius: 6,
+          }}
+        >
           <Div
             css={{
-              gridArea: "gallery",
+              background: "$sand9",
+              position: "absolute",
+              inset: 0,
               width: "100%",
-              position: "relative",
-              height: 0,
-              paddingTop: "75%",
-              overflow: "hidden",
-              borderRadius: 6,
+              height: "100%",
             }}
-          >
-            <Div
-              css={{
-                background: "$sand9",
-                position: "absolute",
-                inset: 0,
-                width: "100%",
-                height: "100%",
-              }}
-            ></Div>
-          </Div>
-          <IconButton css={{ gridArea: "prev", color: "$textSecondary" }}>
-            <ArrowLeftIcon />
-          </IconButton>
-          <Div
-            css={{
-              gridArea: "dots",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            {dots.map((dot) => (
-              <Dot key={dot} active={dot === 3} />
-            ))}
-          </Div>
-          <IconButton css={{ gridArea: "next", color: "$textSecondary" }}>
-            <ArrowRightIcon />
-          </IconButton>
-        </Display>
-        <Div css={{ gridArea: "links" }}>
-          <Text secondary css={{ fontSize: 14, marginBottom: 8 }}>
-            Check out:
-          </Text>
-          <Flex>
-            <Button
-              css={{ flex: 1, marginRight: 8, "& svg": { marginRight: 12 } }}
-            >
-              <GitHubLogoIcon /> Github
-            </Button>
-            <Button css={{ flex: 1, "& svg": { marginRight: 12 } }}>
-              <GlobeIcon /> Preview
-            </Button>
-          </Flex>
+          ></Div>
         </Div>
-        <Div css={{ gridArea: "type" }}>
-          <Text secondary css={{ fontSize: 14, marginBottom: 8 }}>
-            Project type:
-          </Text>
-          <ProjectType>Small project</ProjectType>
+        <IconButton css={{ gridArea: "prev", color: "$textSecondary" }}>
+          <ArrowLeftIcon />
+        </IconButton>
+        <Div
+          css={{
+            gridArea: "dots",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {dots.map((dot) => (
+            <Dot key={dot} active={dot === 3} />
+          ))}
         </Div>
-        <Div css={{ gridArea: "description" }}>
-          <Text as="h5" heading css={{ fontSize: 24, marginBottom: 12 }}>
-            Description
-          </Text>
-          <Text secondary css={{ lineHeight: 1.6 }}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum
-            voluptates amet autem, perspiciatis, voluptatibus, reprehenderit
-            eveniet cumque officiis inventore reiciendis animi id optio quod.
-            Alias magnam aperiam dicta rerum cum unde culpa voluptatum error
-            obcaecati? Rem, expedita vero labore veniam saepe minus iusto
-            recusandae? Quas ullam soluta harum molestiae omnis!
-          </Text>
-        </Div>
-        <Div css={{ gridArea: "skills" }}>
-          <Text secondary css={{ fontSize: 14, marginBottom: 8 }}>
-            Tech used
-          </Text>
-        </Div>
-      </Grid>
+        <IconButton css={{ gridArea: "next", color: "$textSecondary" }}>
+          <ArrowRightIcon />
+        </IconButton>
+      </Display>
+      <Div css={{ marginBottom: 16 }}>
+        <Text as="h5" heading css={{ fontSize: 24, marginBottom: 12 }}>
+          Description
+        </Text>
+        <Text secondary css={{ lineHeight: 1.6 }}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum
+          voluptates amet autem, perspiciatis, voluptatibus, reprehenderit
+          eveniet cumque officiis inventore reiciendis animi id optio quod.
+          Alias magnam aperiam dicta rerum cum unde culpa voluptatum error
+          obcaecati? Rem, expedita vero labore veniam saepe minus iusto
+          recusandae? Quas ullam soluta harum molestiae omnis!
+        </Text>
+        <Text secondary css={{ lineHeight: 1.6 }}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum
+          voluptates amet autem, perspiciatis, voluptatibus, reprehenderit
+          eveniet cumque officiis inventore reiciendis animi id optio quod.
+          Alias magnam aperiam dicta rerum cum unde culpa voluptatum error
+          obcaecati? Rem, expedita vero labore veniam saepe minus iusto
+          recusandae? Quas ullam soluta harum molestiae omnis!
+        </Text>
+        <Text
+          secondary
+          css={{ fontSize: 14, fontStyle: "italic", marginTop: 12 }}
+        >
+          Finished: 21/01/2022
+        </Text>
+      </Div>
+      <Div>
+        <Text heading secondary css={{ fontSize: 14 }}>
+          Tech used
+        </Text>
+        <Flex css={{ flexFlow: "row wrap", width: "100%" }}>
+          {skills.map((skill) => (
+            <Skill key={skill}>{skill}</Skill>
+          ))}
+        </Flex>
+      </Div>
     </Container>
   );
 };
