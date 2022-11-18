f6e30ee1ac6665485ebd636dab790fc87edca15e
Jean Paul Tivan <jptivan53@gmail.com>
2022-11-17 21:06:55 -0400

    ADD: Tooltip y link pag de proyectos por skill

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,6 +1,10 @@
+import { ReactNode } from "react";
 import NextImage from "next/image";
+import Link from "next/link";
+import * as Tooltip from "@radix-ui/react-tooltip";
 import { styled, keyframes } from "@config/stitches.config";
 import Card from "@components/Card";
+import { TSkill } from "@src/types";
 
 const spin = keyframes({
   "0%": {
@@ -23,6 +27,7 @@ const Name = styled("span", {
 });
 
 const Container = styled(Card, {
+  textDecoration: "none",
   padding: 10,
   display: "flex",
   flexDirection: "column",
@@ -44,22 +49,67 @@ const Container = styled(Card, {
   },
 });
 
-const Skill = ({ name, image }: { name: string; image: string }) => {
+{
+  /* <Tooltip.Root>
+  <Tooltip.Trigger asChild>
+    <button className="IconButton">
+      <PlusIcon />
+    </button>
+  </Tooltip.Trigger>
+  <Tooltip.Portal>
+    <Tooltip.Content className="TooltipContent" sideOffset={5}>
+      Add to library
+      <Tooltip.Arrow className="TooltipArrow" />
+    </Tooltip.Content>
+  </Tooltip.Portal>
+</Tooltip.Root>; */
+}
+
+const TooltipArrow = styled(Tooltip.Arrow, {
+  fill: "$sand12",
+});
+
+const StyledTooltipContent = styled(Tooltip.Content, {
+  padding: "6px 12px",
+  backgroundColor: "$sand12",
+  color: "$sand1",
+  zIndex: 99,
+  borderRadius: 4,
+});
+
+const TooltipContent = ({ children }: { children: ReactNode }) => {
+  return (
+    <StyledTooltipContent sideOffset={5}>
+      {children}
+      <TooltipArrow className="TooltipArrow" />
+    </StyledTooltipContent>
+  );
+};
+
+const Skill = ({ name, image, slug }: TSkill) => {
   const isReact = name === "React";
 
   return (
-    <Container
-      css={{ gridArea: name.toLowerCase().replace(" ", "-") }}
-      rotate={isReact}
-    >
-      <Image
-        alt={`${name} logo`}
-        src={image}
-        height={isReact ? 80 : 40}
-        width={isReact ? 80 : 40}
-      />
-      {isReact && <Name>React.js</Name>}
-    </Container>
+    <Tooltip.Root>
+      <Link href={`/projects/skill/${slug}`} legacyBehavior passHref>
+        <Tooltip.Trigger asChild>
+          <Container
+            css={{ gridArea: name.toLowerCase().replace(" ", "-") }}
+            rotate={isReact}
+            as="a"
+          >
+            <Image
+              alt={`${name} logo`}
+              src={image}
+              height={isReact ? 80 : 40}
+              width={isReact ? 80 : 40}
+            />
+            {isReact && <Name>React.js</Name>}
+          </Container>
+        </Tooltip.Trigger>
+      </Link>
+      <TooltipContent>{name}</TooltipContent>
+    </Tooltip.Root>
   );
 };
 
