6d5c738014748003bf4fa851c7e9e40bd17a9f80
Jean Paul Tivan <jptivan53@gmail.com>
2022-11-23 19:36:21 -0400

    CHANGE: Formulario de contacto es su propio comp

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -5,6 +5,7 @@ import Container from "@components/Container";
 import Button from "@components/Button";
 import Text from "@components/Text";
 import Flex from "@components/Flex";
+import ContactForm from "@components/ContactForm";
 
 const Span = styled("span", {});
 const Grid = styled("div", {
@@ -25,61 +26,6 @@ const Content = styled("div", {
 
 const Title = styled("div", {
   marginBottom: 16,
-});
-
-const InputContainer = styled("div", {
-  display: "flex",
-  flexDirection: "column",
-});
-
-const Label = styled("label", {
-  color: "$text",
-  fontFamily: "$mont",
-  fontWeight: 700,
-  marginBottom: 8,
-  display: "block",
-});
-
-const Input = styled("input", {
-  all: "unset",
-  flex: 1,
-  padding: "12px 20px",
-  background: "$sand3",
-  borderRadius: 4,
-  borderColor: "1px solid $sand7",
-  color: "$text",
-
-  "&:hover": {
-    background: "$sand4",
-  },
-
-  "&:focus": {
-    outline: "1px solid $primary",
-  },
-
-  "&:invalid": {
-    outline: "1px solid red",
-  },
-});
-
-const Form = styled("form", {
-  display: "grid",
-  gridGap: "16px",
-  gridTemplateAreas: `
-    "name"
-    "email"
-    "message"
-    "button"
-  `,
-  marginBottom: 64,
-
-  "@md": {
-    gridTemplateAreas: `
-    "name email"
-    "message message"
-    "button button"
-  `,
-  },
 });
 
 const Contact: NextPage = () => {
@@ -125,36 +71,7 @@ const Contact: NextPage = () => {
           >
             Or you can leave me a brief message so I can get back to you
           </Text>
-          <Form>
-            <InputContainer css={{ gridArea: "name" }}>
-              <Label htmlFor="name">Full name</Label>
-              <Input name="name" id="name" placeholder="John Doe" required />
-            </InputContainer>
-            <InputContainer css={{ gridArea: "email" }}>
-              <Label htmlFor="email">Email</Label>
-              <Input
-                name="email"
-                id="email"
-                type="email"
-                placeholder="johndoe@example.com"
-                required
-              />
-            </InputContainer>
-            <InputContainer css={{ gridArea: "message" }}>
-              <Label htmlFor="message">Message</Label>
-              <Input
-                as="textarea"
-                name="message"
-                id="message"
-                css={{ minHeight: 200 }}
-                placeholder="Your message goes here..."
-                required
-                minLength={5}
-                maxLength={256}
-              ></Input>
-            </InputContainer>
-            <Button css={{ gridArea: "button" }}>Send message</Button>
-          </Form>
+          <ContactForm />
         </Content>
       </Grid>
     </Container>
