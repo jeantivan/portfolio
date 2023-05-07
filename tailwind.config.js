9717b3b18e4dac49b3dea55d0d115f02dc2b8a5d
jeantivan <jptivan53@gmail.com>
2023-05-07 00:06:20 -0400

    ADD: Modo oscuro usando clases. Agregados mas colores al tema

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,40 @@ module.exports = {
 
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
+      colors: {
+        primary: {
+          1: "var(--lime1)",
+          2: "var(--lime2)",
+          3: "var(--lime3)",
+          4: "var(--lime4)",
+          5: "var(--lime5)",
+          6: "var(--lime6)",
+          7: "var(--lime7)",
+          8: "var(--lime8)",
+          9: "var(--lime9)",
+          10: "var(--lime10)",
+          11: "var(--lime11)",
+          12: "var(--lime12)",
+        },
+        gold: {
+          1: "var(--gold1)",
+          2: "var(--gold2)",
+          3: "var(--gold3)",
+          4: "var(--gold4)",
+          5: "var(--gold5)",
+          6: "var(--gold6)",
+          7: "var(--gold7)",
+          8: "var(--gold8)",
+          9: "var(--gold9)",
+          10: "var(--gold10)",
+          11: "var(--gold11)",
+          12: "var(--gold12)",
+        },
+      },
+
       fontFamily: {
         sans: ["var(--space-grotesk)"],
       },
