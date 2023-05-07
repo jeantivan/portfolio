06ea4f7838fd64e968c0c341fa23ec4030555401
jeantivan <jptivan53@gmail.com>
2023-05-06 23:21:03 -0400

    ADD: Nueva fuente "Space Grotesk" optimizada

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { Space_Grotesk } from "next/font/google";
 import { getCssText } from "@config/stitches.config";
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  variable: "--space-grotesk",
+});
 
 export default function Document() {
   return (
-    <Html>
+    <Html className={spaceGrotesk.variable}>
       <Head>
         <style
           id="stitches"
