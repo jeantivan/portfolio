9e0f51112485066b232e694af74c0ac544f21de8
jeantivan <jptivan53@gmail.com>
2023-05-08 01:37:23 -0400

    ADD: Input para los formularios

---
+++ src/components/common/Input.tsx
@@ -0,0 +1,44 @@
+import { ComponentProps, ReactNode } from "react";
+import cx from "classnames";
+
+type InputProps = ComponentProps<"input"> &
+  ComponentProps<"textarea"> & {
+    label: string;
+    name: string;
+    error?: string;
+    textarea?: boolean;
+  };
+
+function Input({ label, name, error, textarea, ...rest }: InputProps) {
+  const Component = textarea ? "textarea" : "input";
+
+  return (
+    <div className="flex flex-col w-full h-full">
+      <label
+        className={cx(
+          "capitalize mb-1",
+          error ? "text-red-600" : "dark:text-gold-11"
+        )}
+        htmlFor={name}
+      >
+        {label}
+        <span className="text-primary-9">:</span>
+      </label>
+      <Component
+        className={cx(
+          "rounded-sm border border-gold-5 text-lg w-full",
+          "bg-gold-3 p-3 placeholder:text-gold-8 text-gold-12",
+          "caret-primary-9 outline-none focus:border-primary-9 focus:ring-primary-9 h-full",
+          textarea && "resize-y",
+          error && "border-red-600 mb-1"
+        )}
+        name={name}
+        aria-invalid={Boolean(error)}
+        {...rest}
+      />
+      {error && <p className="text-red-600 text-sm">{error}</p>}
+    </div>
+  );
+}
+
+export default Input;
