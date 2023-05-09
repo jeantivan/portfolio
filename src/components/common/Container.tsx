a10c9260b573c5c0ce6e0abc2f966571b6e65324
jeantivan <jptivan53@gmail.com>
2023-05-09 18:33:37 -0400

    FIX: Prop "className" es opcional

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 import cx from "classnames";
 type ContainerProps = {
   children: ReactNode;
-  className: string;
+  className?: string;
 };
 function Container({ children, className, ...rest }: ContainerProps) {
   return (
