7848f2236612599724752724ac7a679144cfc214
jeantivan <jptivan53@gmail.com>
2023-05-14 02:37:48 -0400

    ADD: Component para mostrar un Tooltip

---
+++ src/components/common/Tooltip.tsx
@@ -0,0 +1,41 @@
+"use client";
+import type { ReactNode } from "react";
+import * as TooltipPrimitive from "@radix-ui/react-tooltip";
+
+type TooltipProps = TooltipPrimitive.TooltipContentProps & {
+  content: ReactNode;
+  delayDuration?: number;
+  forceMount?: boolean;
+};
+
+const ToolTip = ({
+  className,
+  content,
+  delayDuration = 350,
+  sideOffset = 4,
+  align = "center",
+  forceMount,
+  children,
+  ...rest
+}: TooltipProps) => {
+  return (
+    <TooltipPrimitive.Provider delayDuration={delayDuration}>
+      <TooltipPrimitive.Root>
+        <TooltipPrimitive.Trigger asChild>{children}</TooltipPrimitive.Trigger>
+        <TooltipPrimitive.Portal forceMount={forceMount}>
+          <TooltipPrimitive.Content
+            className="select-none rounded shadow-md text-gold-1 bg-gold-12 py-2 px-3 origin-[--radix-tooltip-content-transform-origin]"
+            sideOffset={sideOffset}
+            align="center"
+            {...rest}
+          >
+            {content}
+            <TooltipPrimitive.Arrow className="fill-gold-12" />
+          </TooltipPrimitive.Content>
+        </TooltipPrimitive.Portal>
+      </TooltipPrimitive.Root>
+    </TooltipPrimitive.Provider>
+  );
+};
+
+export { ToolTip, type TooltipProps };
