17a955a8d42b55bf2ce00c70a73599cdbe82b2d5
jeantivan <jptivan53@gmail.com>
2023-05-13 20:53:36 -0400

    ADD: Types para soportar polimorfismo en los componentes

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -51,3 +51,33 @@ export type TContactFormValues = {
   email: string;
   message: string;
 };
+
+// Polimorfismo de componentes
+// Fuente: https://blog.logrocket.com/build-strongly-typed-polymorphic-components-react-typescript/
+
+// Prop as será cualquier tipo de Elemento React
+type AsProp<C extends React.ElementType> = {
+  as?: C;
+};
+
+// Intersección de las props que serán omitidas usando Omit
+type PropsToOmit<C extends React.ElementType, P> = keyof (AsProp<C> & P);
+
+// Extiende las propiedades del Componente
+// agrega children y cualquier prop aceptada por "as"
+// ejemplo: si as = a -> "href" será prop valida
+// Nota: Si las props del Componente también son validas por la etiqueta HTML el type estará dado por la prop del Component
+export type PolymorphicComponentProps<
+  C extends React.ElementType,
+  Props = {}
+> = React.PropsWithChildren<Props & AsProp<C>> &
+  Omit<React.ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>;
+
+export type PolymorphicRef<C extends React.ElementType> =
+  React.ComponentPropsWithRef<C>["ref"];
+
+// Extendemos las props para que el component pueda aceptar una ref
+export type PolymorphicComponentPropsWithRef<
+  C extends React.ElementType,
+  Props = {}
+> = PolymorphicComponentProps<C, Props> & { ref?: PolymorphicRef<C> };
