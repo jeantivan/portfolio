23b3b17fb08030c66b797834346ada60eff81668
jeantivan <jptivan53@gmail.com>
2023-05-15 14:49:15 -0400

    ADD: Maquetado de la pagina de contacto

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,7 @@
+import Button from "@components/common/Button";
 import Container from "@components/common/Container";
 import Text from "@components/common/Text";
+import Input from "@components/common/Input";
 
 export const metadata = {
   title: "Contact | JeanTivan.dev",
@@ -9,11 +11,63 @@ export const metadata = {
 function Contact() {
   return (
     <Container className="my-8 md:my-16">
-      <Text Component="h1" variant="title" className="uppercase">
-        Let<span className="text-primary-9">{"'"}</span>s connect and work
-        together
-        <span className="text-primary-9">!</span>
-      </Text>
+      <section className="mb-16 md:mb-20">
+        <Text Component="h1" variant="title" className="uppercase mb-6 md:mb-8">
+          Let<span className="text-primary-9">{"'"}</span>s connect and work
+          together
+          <span className="text-primary-9">!</span>
+        </Text>
+        <Text variant="body2" color="secondary" className="mb-14">
+          Lorem ipsum dolor sit amet consectetur. Pharetra a in aliquet
+          malesuada. Id interdum cursus mi in. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Possimus, quas?
+        </Text>
+        <div className="flex flex-wrap gap-4">
+          <Button color="primary" variant="outlined" className="flex-1">
+            Linkedin
+          </Button>
+          <Button color="primary" variant="outlined" className="flex-1">
+            Email
+          </Button>
+          <Button color="primary" variant="outlined" className="flex-1">
+            Upwork
+          </Button>
+        </div>
+      </section>
+      <section>
+        <Text Component="h2" variant="body2" className="uppercase mb-6">
+          Also you can sent me a message using the next form
+        </Text>
+        <div className="flex flex-wrap gap-4">
+          <div className="flex-1 md:w-1/2">
+            <Input
+              label="Full name"
+              name="fullName"
+              placeholder="Enter your name"
+            />
+          </div>
+          <div className="flex-1 md:w-1/2">
+            <Input
+              label="Email"
+              name="email"
+              type="email"
+              placeholder="youremail@example.com"
+            />
+          </div>
+          <div className="w-full">
+            <Input
+              label="Message"
+              name="message"
+              textarea
+              type="email"
+              placeholder="Start typing your message here..."
+            />
+          </div>
+          <div className="flex-1">
+            <Button className="w-full">Send message</Button>
+          </div>
+        </div>
+      </section>
     </Container>
   );
 }
