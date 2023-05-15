aadc1d0b5422b89eba5630ec48d2a83e52ca8e0f
jeantivan <jptivan53@gmail.com>
2023-05-15 13:11:38 -0400

    ADD: Nuevos estilos

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -13,10 +13,10 @@ function Input({ label, name, error, textarea, ...rest }: InputProps) {
   const Component = textarea ? "textarea" : "input";
 
   return (
-    <div className="flex flex-col w-full h-full">
+    <div className={cx("w-full flex flex-wrap", textarea && "h-full")}>
       <label
         className={cx(
-          "capitalize mb-1",
+          "capitalize mb-1 block flex-1",
           error ? "text-red-600" : "text-gold-11"
         )}
         htmlFor={name}
@@ -28,8 +28,8 @@ function Input({ label, name, error, textarea, ...rest }: InputProps) {
         className={cx(
           "rounded-sm border border-gold-5 text-lg w-full",
           "bg-gold-3 p-3 placeholder:text-gold-8 text-gold-12",
-          "caret-primary-9 outline-none focus:border-primary-9 focus:ring-primary-9 h-full",
-          textarea && "resize-y",
+          "caret-primary-9 outline-none focus:border-primary-9 focus:ring-primary-9",
+          textarea && "resize-y min-h-[8rem]",
           error && "border-red-600 mb-1"
         )}
         name={name}
