4873276da0e1b7b8548e5abbbf0f3fdf832a55b8
jeantivan <jptivan53@gmail.com>
2023-05-16 14:51:17 -0400

    CHANGE:: No se usa el AccessibleIcon de @radix-ui Se copió su funcionalidad

--- src/components/common/AccesibleIcon.tsx
+++ src/components/common/AccesibleIcon.tsx
@@ -1,17 +1,42 @@
-"use client";
-
-import * as AccesibleIconPrimitive from "@radix-ui/react-accessible-icon";
+import type { ElementType, CSSProperties } from "react";
 import cx from "classnames";
 
-type AccesibleIconProps = AccesibleIconPrimitive.AccessibleIconProps & {
-  className?: string;
+// Visually Hidden for screens
+// Source:  https://github.com/radix-ui/primitives/blob/main/packages/react/visually-hidden/src/VisuallyHidden.tsx
+const screenReaderStyle: CSSProperties = {
+  position: "absolute",
+  border: 0,
+  width: 1,
+  height: 1,
+  padding: 0,
+  margin: -1,
+  overflow: "hidden",
+  clip: "rect(0, 0, 0, 0)",
+  whiteSpace: "nowrap",
+  wordWrap: "normal",
 };
-function AccesibleIcon({ className, children, label }: AccesibleIconProps) {
+
+type AccesibleIconProps = {
+  label: string;
+  icon: ElementType;
+  className?: string;
+  iconClassName?: string;
+};
+function AccesibleIcon({
+  className,
+  label,
+  icon,
+  iconClassName,
+}: AccesibleIconProps) {
+  const Icon = icon || "svg";
   return (
-    <span className={cx("fill-current", className)}>
-      <AccesibleIconPrimitive.Root label={label}>
-        {children}
-      </AccesibleIconPrimitive.Root>
+    <span className={cx("inline-flex w-full h-full", className)}>
+      <Icon
+        className={cx("fill-current w-full h-full", iconClassName)}
+        aria-hidden
+        focusable={false}
+      />
+      <span style={{ ...screenReaderStyle }}>{label}</span>
     </span>
   );
 }
