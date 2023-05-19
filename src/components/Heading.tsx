50fb707db75f47009456dbe38040476fadfca616
jeantivan <jptivan53@gmail.com>
2023-05-18 22:38:11 -0400

    NEW: Resalta caracteres especiales en un titulo

---
+++ src/components/Heading.tsx
@@ -0,0 +1,29 @@
+import Text from "./common/Text";
+import type { TextProps } from "./common/Text";
+
+import { splitEspecialChar, EspecialCharRgx } from "@utils/splitEspecialChar";
+
+type HeadingProps = Omit<TextProps, "children" | "variant"> & {
+  text: string;
+  variant?: "title" | "heading" | "subheading";
+};
+function Heading({ text, variant = "subheading", ...rest }: HeadingProps) {
+  const textToRender = splitEspecialChar(text).map((str, i) => {
+    if (str.search(EspecialCharRgx) >= 0) {
+      return (
+        <span key={`sing-${i}`} className="text-primary-9">
+          {str}
+        </span>
+      );
+    }
+    return str;
+  });
+
+  return (
+    <Text variant={variant} {...rest}>
+      {textToRender}
+    </Text>
+  );
+}
+
+export default Heading;
