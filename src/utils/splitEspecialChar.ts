9dc746e5f426ffd382a16d575be2d49028f603c1
jeantivan <jptivan53@gmail.com>
2023-05-18 22:37:17 -0400

    FIX: Añade el resto de la string si no terminó con carácter especial

--- src/utils/splitEspecialChar.ts
+++ src/utils/splitEspecialChar.ts
@@ -42,5 +42,41 @@ export const splitEspecialChar = (
     currStr = currStr.slice(indexOfEspChar + 1);
   }
 
+  // Añade el restante de la string si no terminaba con carácter especial.
+  if (currStr.length > 0) {
+    splitStr.push(currStr);
+  }
+
   return splitStr;
 };
+
+/* 
+const splitEspecialChar = (str, rgx = /[^\w \xC0-\xFF]+/g) => {
+  // Acá se guardará el string dividido
+  let splitStr = [];
+
+  // Copia del string a dividir
+  let currStr = str;
+
+  // Cadena separada sin incluir los caracteres especial.
+  while (currStr.search(rgx) >= 0) {
+    // Carácter en el index
+    let indexOfEspChar = currStr.search(rgx);
+
+    // No agrega
+    if (indexOfEspChar !== 0) {
+      // Añade la string que está antes del carácter
+      splitStr.push(currStr.slice(0, indexOfEspChar));
+    }
+
+    // Añade el signo el carácter al array
+    splitStr.push(currStr[indexOfEspChar]);
+
+    // Cambia el valor de la cadena actual hasta donde se encontró el carácter
+    currStr = currStr.slice(indexOfEspChar + 1);
+  }
+
+  console.log("CurrStr Length: " + currStr.length)
+  return splitStr;
+}; 
+*/
