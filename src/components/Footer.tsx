c39e2a87c288da51601058a5b44a7a253d5bf026
jeantivan <jptivan53@gmail.com>
2023-10-19 15:58:57 +0200

    ADD: Sección de Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <div className="flex p-4 justify-center">
+      <p className="text-background-12">
+        Checkout my{" "}
+        <a
+          href="https://github.com/jeantivan"
+          className="text-primary-9 no-underline hover:underline underline-offset-2"
+        >
+          Github profile!
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
