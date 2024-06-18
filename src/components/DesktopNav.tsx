b0452f654ad1980dc4c8762666a801420c3d6722
jeantivan <jptivan53@gmail.com>
2024-06-19 00:25:07 +0200

    WIP: Implementando el diseño final de la Navbar para desktop

--- src/components/DesktopNav.tsx
+++ src/components/DesktopNav.tsx
@@ -2,12 +2,14 @@ import { ComponentPropsWithoutRef } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { DownloadIcon, GitHubLogoIcon, MoonIcon } from "@radix-ui/react-icons";
 
 import { mc } from "@/utils/helpers";
-import Logo from "./Logo";
+
 import IconButton from "./common/IconButton";
 import { Tooltip } from "./common/Tooltip";
+import Text from "./common/Text";
+import Button from "./common/Button";
 
 type NavLinkProps = ComponentPropsWithoutRef<typeof Link> & {
   isActive?: boolean;
@@ -18,13 +20,25 @@ function NavLink({ href, isActive, ...props }: NavLinkProps) {
       {...props}
       href={href}
       className={mc(
-        "uppercase py-2 px-4 ",
+        "uppercase text-base py-2 px-5",
         "relative z-0 text-background-12 hover:text-primary-11",
         isActive && "text-primary-9 hover:text-primary-9"
       )}
     >
-      <span className="px-2 bg-inherit">{props.children}</span>
+      <span className="bg-inherit">{props.children}</span>
     </Link>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="flex-1 md:flex-initial text-3xl md:text-2xl text-gold-12">
+      <Link href="/">
+        <Text className="text-xl leading-[1.25rem] uppercase" display>
+          JeanTivan
+        </Text>
+      </Link>
+    </div>
   );
 }
 
@@ -32,9 +46,9 @@ function DesktopNav() {
   const pathname = usePathname();
 
   return (
-    <div className="hidden md:flex items-center py-2">
-      <Logo />
-      <nav className="hidden md:flex flex-1 justify-center gap-8">
+    <div className="hidden md:flex items-center py-3">
+      {pathname !== "/" ? <Logo /> : null}
+      <nav className="hidden md:flex flex-1 justify-end gap-4">
         <NavLink href="/" isActive={pathname === "/"}>
           About
         </NavLink>
@@ -44,16 +58,18 @@ function DesktopNav() {
         <NavLink href="/contact" isActive={pathname === "/contact"}>
           Contact
         </NavLink>
+        <Button as="a">
+          Descargar CV{" "}
+          <span className="inline-flex ml-2.5">
+            <DownloadIcon />
+          </span>
+        </Button>
+        <div className="flex gap-2">
+          <Tooltip content="Switch color mode">
+            <IconButton label="Switch color mode" icon={MoonIcon} />
+          </Tooltip>
+        </div>
       </nav>
-      <div className="flex gap-2 -mr-2.5">
-        <Tooltip content="Visit my github">
-          <IconButton
-            label="Visit my github"
-            color="secondary"
-            icon={GitHubLogoIcon}
-          />
-        </Tooltip>
-      </div>
     </div>
   );
 }
