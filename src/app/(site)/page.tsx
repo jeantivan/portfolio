d1470f1a1441dcce0f8ae5dede50126c46791f9e
jeantivan <jptivan53@gmail.com>
2024-06-19 21:20:20 +0200

    UPDATE: Terminado el maquetado e implementación del diseño de la pag

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from "next";
 import Link from "next/link";
 import { ArrowRightIcon } from "@radix-ui/react-icons";
 
@@ -8,9 +9,11 @@ import Button from "@/components/common/Button";
 import Heading from "@/components/Heading";
 import FeaturedProject from "@/src/components/FeaturedProject";
 import { getProjects } from "@/src/sanity/queries";
+
+import Skill from "@/src/components/Skill";
 import Card from "@/src/components/common/Card";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "About",
   description:
     "Welcome to my portfolio, my name is Jean Tivan a self-taught frontend developer passionate for react and next.js"
@@ -21,11 +24,11 @@ async function Home() {
 
   const featuredProjects = projects
     .filter((project) => project.isFeatured)
-    .slice(2);
+    .slice(0, 3);
   return (
-    <Container className="px-3 py-6 lg:py-16">
-      <section className="mb-14">
-        <div className="mb-4 md:mb-8 flex flex-col md:flex-row gap-6 justify-between">
+    <Container className="px-3 py-6 lg:py-10 grid gap-16 md:gap-24">
+      <section className="">
+        <div className="mb-4 flex flex-col md:flex-row gap-6 justify-end">
           {/* TODO: Implementar imagen
           <Card className="bg-background-11 w-full rounded-xl mb-5 md:mb-0 lg:h-full aspect-video md:aspect-[2/3]"></Card> */}
           <div className="shrink-0 flex flex-col justify-end">
@@ -49,34 +52,92 @@ async function Home() {
             </Text>
           </div>
         </div>
-        <div className="mb-7 md:mb-9">
-          <Text variant="subheading" weight="light" color="secondary">
-            Lorem ipsum dolor sit amet consectetur. Eget sed sed quam faucibus
-            mi in nisl.
-          </Text>
+        <Text variant="subheading" weight="light" color="secondary">
+          Lorem ipsum dolor sit amet consectetur. Eget sed sed quam sit nisl.
+        </Text>
+      </section>
+      <section className="grid gap-6 md:gap-8">
+        <Heading
+          text="The latest projects that I've build."
+          Component="h3"
+          variant="title"
+          className="flex-1 text-center"
+        />
+        <div className="flex flex-wrap justify-center gap-3">
+          {featuredProjects.map((project, index) => (
+            <FeaturedProject key={project._id} project={project} />
+          ))}
+        </div>
+        <div className="flex justify-center">
+          <Button
+            className="w-full max-w-lg"
+            variant="outlined"
+            as={Link}
+            href="/projects"
+          >
+            Checkout all my projects <ArrowRightIcon className="w-4 h-4" />
+          </Button>
         </div>
       </section>
-      <section className="mb-20 md:mb-32">
-        <div className="flex flex-col md:flex-row gap-4">
-          <div className="border border-primary-6 rounded flex-1 p-6 md:p-12 flex flex-col">
-            <Heading
-              text="The latest projects that I've build."
-              Component="h3"
-              variant="title"
-              className="flex-1 mb-20 md:mb-12"
+      <section className="grid gap-6 md:gap-8">
+        <Heading
+          text="My favorites tech to build experiences."
+          variant="heading"
+          display
+        />
+        <div className="grid grid-cols-2 md:grid-cols-4 justify-between gap-3">
+          <Card className="max-w-60 bg-background-2 px-8 py-4">
+            <Skill
+              showName
+              skill={{ name: "Next.js", slug: { current: "" } }}
             />
-            <div className="flex justify-end flex-none">
-              <Button as={Link} href="/projects">
-                See all the projects{" "}
-                <ArrowRightIcon className="w-5 h-5 fill-current" />
-              </Button>
-            </div>
-          </div>
-          <div className="flex flex-1 flex-col gap-4">
-            {featuredProjects.map((project) => (
-              <FeaturedProject key={project._id} project={project} />
-            ))}
-          </div>
+          </Card>
+          <Card className="max-w-60 bg-background-2 px-8 py-4">
+            <Skill
+              showName
+              skill={{ name: "React.js", slug: { current: "" } }}
+            />
+          </Card>
+          <Card className="max-w-60 bg-background-2 px-8 py-4">
+            <Skill
+              showName
+              skill={{ name: "TypeScript", slug: { current: "" } }}
+            />
+          </Card>
+          <Card className="max-w-60 bg-background-2 px-8 py-4">
+            <Skill
+              showName
+              skill={{ name: "Tailwind", slug: { current: "" } }}
+            />
+          </Card>
+        </div>
+        <div className="flex justify-end">
+          <Button
+            className="w-full max-w-lg"
+            variant="outlined"
+            as={Link}
+            href="/skills"
+          >
+            Checkout all my skills <ArrowRightIcon className="w-4 h-4" />
+          </Button>
+        </div>
+      </section>
+      <section className="grid gap-6 md:gap-8">
+        <Heading text="About me." variant="heading" display />
+        <Text variant="body2" color="secondary">
+          Lorem ipsum dolor sit amet consectetur. Morbi blandit morbi enim sit
+          sed. Arcu non hac enim turpis ullamcorper quam eget turpis. Proin est
+          turpis ut in sit. Semper elit ut vel fermentum volutpat lacus mattis
+          aliquam et. Pretium diam ultrices tincidunt cursus nunc et congue
+          nibh. Fames non libero turpis augue ultrices. Molestie imperdiet id at
+          nisi nisl. Fringilla congue adipiscing quam etiam sodales curabitur
+          purus in. Leo sed amet senectus vel dui id vulputate cursus. Sit in
+          molestie enim commodo adipiscing purus.
+        </Text>
+        <div className="flex justify-end">
+          <Button as={Link} href="/contact">
+            Get in touch with me
+          </Button>
         </div>
       </section>
     </Container>
