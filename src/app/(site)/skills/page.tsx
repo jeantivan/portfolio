226b8041e3e48c477d3e07ad0bdf858f489b7f0e
jeantivan <jptivan53@gmail.com>
2024-06-19 21:19:29 +0200

    WIP: Pagina para mostrar todas las skills

---
+++ src/app/(site)/skills/page.tsx
@@ -0,0 +1,12 @@
+import Container from "@/src/components/common/Container";
+import Heading from "@/src/components/Heading";
+
+async function Skills() {
+  return (
+    <Container>
+      <Heading text="Skills page." variant="title" />
+    </Container>
+  );
+}
+
+export default Skills;
