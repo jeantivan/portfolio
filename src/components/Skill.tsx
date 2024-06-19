19d03f0ad177276d50431c32b42f60daff0f6ca9
jeantivan <jptivan53@gmail.com>
2024-06-19 20:42:39 +0200

    UPDATE: Reescrito el componente WIP

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,118 +1,20 @@
-import { ReactNode } from "react";
-import NextImage from "next/image";
-import Link from "next/link";
-import * as Tooltip from "@radix-ui/react-tooltip";
+import Card from "./common/Card";
 
-import { styled, keyframes } from "@config/stitches.config";
+import { TSkill } from "@/utils/types";
+import Text from "./common/Text";
 
-import Card from "@components/Card";
-import { TSkill } from "@utils/types";
+type SkillProps = {
+  skill: TSkill;
+  showName: boolean;
+};
 
-const spin = keyframes({
-  "0%": {
-    transform: "rotate(0deg)",
-  },
-  "100%": {
-    transform: "rotate(360deg)",
-  },
-});
-
-const Image = styled(NextImage, {});
-
-const Name = styled("span", {
-  fontSize: 18,
-  marginTop: 12,
-  textAlign: "center",
-  color: "$text",
-  fontFamily: "$mont",
-  fontWeight: "500",
-});
-
-const Container = styled(Card, {
-  textDecoration: "none",
-  padding: 10,
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "space-between",
-  alignItems: "center",
-
-  "&:hover": {
-    boxShadow: "0px 0px 16px $colors$primary",
-  },
-
-  variants: {
-    rotate: {
-      true: {
-        "& img": {
-          animation: `${spin} infinite 20s linear`,
-        },
-      },
-    },
-  },
-});
-
-{
-  /* <Tooltip.Root>
-  <Tooltip.Trigger asChild>
-    <button className="IconButton">
-      <PlusIcon />
-    </button>
-  </Tooltip.Trigger>
-  <Tooltip.Portal>
-    <Tooltip.Content className="TooltipContent" sideOffset={5}>
-      Add to library
-      <Tooltip.Arrow className="TooltipArrow" />
-    </Tooltip.Content>
-  </Tooltip.Portal>
-</Tooltip.Root>; */
+function Skill({ skill, showName = false }: SkillProps) {
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center">
+      <div className="w-28 md:w-32 aspect-square rounded-full bg-background-10"></div>
+      {showName && <Text variant="body2">{skill.name}</Text>}
+    </div>
+  );
 }
-
-const TooltipArrow = styled(Tooltip.Arrow, {
-  fill: "$sand12",
-});
-
-const StyledTooltipContent = styled(Tooltip.Content, {
-  padding: "6px 12px",
-  backgroundColor: "$sand12",
-  color: "$sand1",
-  zIndex: 99,
-  borderRadius: 4,
-});
-
-const TooltipContent = ({ children }: { children: ReactNode }) => {
-  return (
-    <StyledTooltipContent sideOffset={5}>
-      {children}
-      <TooltipArrow className="TooltipArrow" />
-    </StyledTooltipContent>
-  );
-};
-
-const Skill = ({ name, image, slug }: TSkill) => {
-  const isReact = name === "React";
-
-  return (
-    <Tooltip.Root>
-      <Link href={`/projects/skill/${slug}`} legacyBehavior passHref>
-        <Tooltip.Trigger asChild>
-          <Container
-            css={{ gridArea: name.toLowerCase().replace(" ", "-") }}
-            rotate={isReact}
-            as="a"
-          >
-            <Image
-              alt={`${name} logo`}
-              src={image}
-              height={isReact ? 80 : 40}
-              width={isReact ? 80 : 40}
-            />
-            {isReact && <Name>React.js</Name>}
-          </Container>
-        </Tooltip.Trigger>
-      </Link>
-      <TooltipContent>{name}</TooltipContent>
-    </Tooltip.Root>
-  );
-};
 
 export default Skill;
