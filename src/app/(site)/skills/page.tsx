6948ea3fd064e2ec65d14f23a97c7bfa540d089a
jeantivan <jptivan53@gmail.com>
2024-06-25 22:19:59 +0200

    ADD: Implementada la "bento grid" en mobile y desktop

--- src/app/(site)/skills/page.tsx
+++ src/app/(site)/skills/page.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from "react";
 import Card from "@/src/components/common/Card";
 import Container from "@/src/components/common/Container";
 import Text from "@/src/components/common/Text";
@@ -11,97 +12,116 @@ const TempBox = ({ small = false }: { small?: boolean }) => (
   <div
     className={mc(
       "w-28 h-28 rounded-lg bg-background-6",
-      small && "w-[72px] h-[72px]"
+      small && "lg:w-[72px] lg:h-[72px]"
     )}
   ></div>
+);
+
+const SkillsContainer = ({
+  className,
+  children
+}: {
+  className: string;
+  children: ReactNode;
+}) => (
+  <Card
+    className={mc(
+      "py-6 px-3 lg:px-6 grid grid-rows-[min-content_min-content] gap-6",
+      className
+    )}
+  >
+    {children}
+  </Card>
 );
 
 async function Skills() {
   return (
     <Container className="px-3 py-6 lg:py-10 grid gap-16 md:gap-24">
-      <div>
-        <Heading
-          text="Mi caja de herramientas."
-          variant="title"
-          className="text-center mb-4"
-          display
-        />
-        <Heading
-          variant="subheading"
-          text="Tecnologías que potencian mis proyectos."
-          className="text-center"
-        />
-      </div>
-      <div className="grid grid-cols-12 grid-rows-6 gap-4">
-        <Card className="p-6 grid gap-6 bg-background-2 col-start-1 col-end-9 row-start-1 row-end-3">
-          <Text variant="subheading">Main stack</Text>
-          <div className="flex w-full justify-around">
-            <TempBox></TempBox>
-            <TempBox></TempBox>
-            <TempBox></TempBox>
-            <TempBox></TempBox>
-          </div>
-        </Card>
-        <Card className="p-6 grid gap-4 bg-background-2 col-start-9 col-end-13 row-start-1 row-end-4">
-          <Text variant="body2">Fundations</Text>
-          <div className="flex flex-wrap justify-center w-full gap-3">
-            <TempBox></TempBox>
-            <TempBox></TempBox>
-            <TempBox></TempBox>
-          </div>
-        </Card>
-        <Card className="p-6 grid gap-4 justify-center bg-background-2 col-start-1 col-end-6 row-start-3 row-end-5">
-          <Text variant="body2">Styling</Text>
-          <div className="flex justify-center w-full gap-3">
-            <TempBox></TempBox>
-            <TempBox></TempBox>
-            <TempBox></TempBox>
-          </div>
-        </Card>
-
-        <Card className="p-6 grid gap-4 justify-center bg-background-2 col-start-6 col-end-9 row-start-3 row-end-6">
-          <Text variant="body2">Animation</Text>
-          <div className="flex w-full flex-col justify-between">
-            <TempBox></TempBox>
-            <TempBox></TempBox>
-          </div>
-        </Card>
-        <Card className="p-6 grid gap-4 bg-background-2 col-start-9 col-end-13 row-start-4 row-end-6">
-          <Text variant="body2">Control Version</Text>
-          <div className="flex w-full justify-between">
-            <TempBox></TempBox>
-            <TempBox></TempBox>
-          </div>
-        </Card>
-        <Card className="p-6 flex items-end bg-background-2 col-start-1 col-end-6 row-start-5 row-end-6">
-          <Text className="flex-1" variant="subheading" display>
-            Mas herramientas
-          </Text>
-          <ArrowDownIcon className="w-12 h-12 text-primary-9" />
-        </Card>
-        <Card className="p-3 grid items-center bg-background-2 col-start-1 col-end-11 row-start-6 row-end-7">
-          <div className="w-full flex justify-around">
-            <TempBox small></TempBox>
-            <TempBox small></TempBox>
-            <TempBox small></TempBox>
-            <TempBox small></TempBox>
-            <TempBox small></TempBox>
-            <TempBox small></TempBox>
-            <TempBox small></TempBox>
-            <TempBox small></TempBox>
-          </div>
-        </Card>
-        <Card className="p-3 grid items-center justify-center col-start-11 col-end-13 row-start-6 row-end-7">
-          <Text
-            className="tracking-wide -mt-4"
-            variant="heading"
-            color="primary"
+      <section className="grid gap-6">
+        <div>
+          <Heading
+            text="Mi caja de herramientas."
+            variant="title"
+            className="text-center mb-4"
             display
-          >
-            ...
+          />
+          <Text variant="subheading" color="secondary" className="text-center">
+            Tecnologías que potencian mis proyectos.
           </Text>
-        </Card>
-      </div>
+        </div>
+        <div className="grid lg:grid-cols-12 gap-4">
+          <SkillsContainer className="lg:col-start-1 lg:col-end-9 lg:row-start-1 lg:row-end-3">
+            <Text variant="subheading">Main stack</Text>
+            <div className="flex flex-wrap gap-3 w-full justify-center lg:justify-between">
+              <TempBox></TempBox>
+              <TempBox></TempBox>
+              <TempBox></TempBox>
+              <TempBox></TempBox>
+            </div>
+          </SkillsContainer>
+          <SkillsContainer className="lg:col-start-9 lg:col-end-13 lg:row-start-1 lg:row-end-4">
+            <Text variant="body2">Fundations</Text>
+            <div className="flex flex-wrap justify-center w-full gap-3">
+              <TempBox></TempBox>
+              <TempBox></TempBox>
+              <TempBox></TempBox>
+            </div>
+          </SkillsContainer>
+          <SkillsContainer className="justify-center lg:col-start-1 lg:col-end-6 lg:row-start-3 lg:row-end-5">
+            <Text variant="body2">Styling</Text>
+            <div className="flex xs:flex-wrap justify-center w-full gap-3">
+              <TempBox></TempBox>
+              <TempBox></TempBox>
+              <TempBox></TempBox>
+            </div>
+          </SkillsContainer>
+
+          <SkillsContainer className="lg:col-start-6 lg:col-end-9 lg:row-start-3 lg:row-end-6">
+            <Text variant="body2">Animation</Text>
+            <div className="flex w-full lg:flex-col gap-3 justify-center items-center">
+              <TempBox></TempBox>
+              <TempBox></TempBox>
+            </div>
+          </SkillsContainer>
+          <SkillsContainer className="lg:col-start-9 lg:col-end-13 lg:row-start-4 lg:row-end-6">
+            <Text variant="body2">Control Version</Text>
+            <div className="flex w-full gap-3 justify-center lg:justify-center">
+              <TempBox></TempBox>
+              <TempBox></TempBox>
+            </div>
+          </SkillsContainer>
+          <SkillsContainer className="flex items-end lg:col-start-1 lg:col-end-6 lg:row-start-5 lg:lg:row-end-6">
+            <Text className="flex-1" variant="subheading" display>
+              Mas herramientas
+            </Text>
+            <span className="inline-block">
+              <ArrowDownIcon className="w-12 h-12 text-primary-9 rotate-12 lg:-rotate-12" />
+            </span>
+          </SkillsContainer>
+          <SkillsContainer className="items-center lg:col-start-1 lg:col-end-11 lg:row-start-6 lg:lg:row-end-7">
+            <div className="w-full row-span-2 flex flex-wrap items-center lg:justify-around gap-3">
+              <TempBox small></TempBox>
+              <TempBox small></TempBox>
+              <TempBox small></TempBox>
+              <TempBox small></TempBox>
+              <TempBox small></TempBox>
+              <TempBox small></TempBox>
+              <TempBox small></TempBox>
+              <TempBox small></TempBox>
+            </div>
+          </SkillsContainer>
+          <Card className="p-3 grid items-center justify-center lg:col-start-11 lg:lg:col-end-13 lg:row-start-6 lg:lg:row-end-7">
+            <Text
+              className="tracking-wide -mt-4"
+              variant="heading"
+              color="primary"
+              display
+            >
+              ...
+            </Text>
+          </Card>
+        </div>
+      </section>
     </Container>
   );
 }
