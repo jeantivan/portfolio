95db9b3f61105e56762c42d845733bab4d51540c
jeantivan <jptivan53@gmail.com>
2024-06-26 19:23:53 +0200

    CHANGE: Actualizado el builder correspondiente a la nueva version de sanity

--- src/sanity/utils.ts
+++ src/sanity/utils.ts
@@ -1,10 +1,17 @@
-import imageUrlBuilder from "@sanity/image-url";
-import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import createImageUrlBuilder from "@sanity/image-url";
+import type { Image } from "sanity";
 
-import { client } from "./client";
+import { dataset, projectId } from "./api";
 
-const builder = imageUrlBuilder(client);
+const imageBuilder = createImageUrlBuilder({
+  projectId: projectId || "",
+  dataset: dataset || ""
+});
 
-export const imageFor = (source: SanityImageSource) => {
-  return builder.image(source);
+export const urlForImage = (source: Image | undefined) => {
+  if (!source?.asset?._ref) {
+    return undefined;
+  }
+
+  return imageBuilder.image(source).auto("format").fit("max");
 };
