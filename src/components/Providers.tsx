bb317ce0e51f641f50d480004f4ee4fabad84844
jeantivan <jptivan53@gmail.com>
2024-07-11 18:32:05 +0200

    CHANGE: El modo por defecto es el oscuro

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -4,5 +4,9 @@ import { ReactNode } from "react";
 import { ThemeProvider } from "next-themes";
 
 export default function Providers({ children }: { children: ReactNode }) {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+  return (
+    <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
+      {children}
+    </ThemeProvider>
+  );
 }
