cefdedd103e5238f27383dcb93fe91167360321f
jeantivan <jptivan53@gmail.com>
2024-07-11 18:27:30 +0200

    CHANGE: Color de fondo y texto corregidos

--- src/components/common/Tooltip.tsx
+++ src/components/common/Tooltip.tsx
@@ -26,13 +26,13 @@ const Tooltip = ({
         </TooltipPrimitive.Trigger>
         <TooltipPrimitive.Portal forceMount={forceMount}>
           <TooltipPrimitive.Content
-            className="select-none z-[999] rounded-sm shadow-md text-gold-1 bg-gold-12 py-2 px-3 origin-[--radix-tooltip-content-transform-origin]"
+            className="select-none z-[999] rounded-sm shadow-md text-background-12 bg-background-1 py-2 px-3 origin-[--radix-tooltip-content-transform-origin]"
             sideOffset={sideOffset}
             align="center"
             {...rest}
           >
             {content}
-            <TooltipPrimitive.Arrow className="fill-gold-12" />
+            <TooltipPrimitive.Arrow className="fill-background-1" />
           </TooltipPrimitive.Content>
         </TooltipPrimitive.Portal>
       </TooltipPrimitive.Root>
