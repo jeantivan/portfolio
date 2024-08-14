2f0c084fe1f7e1063accb0011a007bc244bcf65d
jeantivan <jptivan53@gmail.com>
2024-08-14 21:46:26 +0200

    ADD: Muestra la imagen y el nombre correspondiente de la Skill

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,7 +1,9 @@
 import Card from "./common/Card";
+import Image from "next/image";
 
 import { TSkill } from "@/utils/types";
 import Text from "./common/Text";
+import { urlForImage } from "../sanity/utils";
 
 type SkillProps = {
   skill: TSkill;
@@ -9,10 +11,20 @@ type SkillProps = {
 };
 
 function Skill({ skill, showName = false }: SkillProps) {
+  const { name, image } = skill;
+
+  const imageUrl = urlForImage(image)?.width(256).url() ?? "";
   return (
     <div className="flex flex-col gap-4 items-center justify-center">
-      <div className="w-28 md:w-32 aspect-square rounded-full bg-background-10"></div>
-      {showName && <Text variant="body2">{skill.name}</Text>}
+      <div className="w-28 md:w-32 aspect-square relative">
+        <Image
+          alt={(image.alt as string) || `${name}`}
+          src={imageUrl}
+          fill
+          className="rounded-xl object-contain"
+        />
+      </div>
+      {showName && <Text variant="body2">{name}</Text>}
     </div>
   );
 }
