f792783bf7679874671eed37b877f15f14470e1a
jeantivan <jptivan53@gmail.com>
2024-08-14 20:33:13 +0200

    ADD: Imagenes e info correspondiente a las Skills

--- src/app/(site)/skills/page.tsx
+++ src/app/(site)/skills/page.tsx
@@ -1,10 +1,16 @@
 import { ReactNode } from "react";
+import { groupBy } from "lodash";
 import Card from "@/src/components/common/Card";
 import Container from "@/src/components/common/Container";
 import Text from "@/src/components/common/Text";
 import Heading from "@/src/components/Heading";
 import { mc } from "@/src/utils/helpers";
 import { ArrowDownIcon } from "@radix-ui/react-icons";
+import { TSkill } from "@/src/utils/types";
+import { getTechSkills } from "@/src/sanity/queries";
+import { urlForImage } from "@/src/sanity/utils";
+import Image from "next/image";
+import { Tooltip } from "@/src/components/common/Tooltip";
 
 // Tecnologias favoritas que uso para construir experiencias
 
@@ -16,6 +22,43 @@ const TempBox = ({ small = false }: { small?: boolean }) => (
     )}
   ></div>
 );
+
+const SkillsImage = ({
+  small = false,
+  skill
+}: {
+  small?: boolean;
+  skill: TSkill;
+}) => {
+  const { name, image } = skill;
+
+  const imageUrl = urlForImage(image)?.width(176).url() ?? "";
+
+  return (
+    <Tooltip content={name}>
+      <div
+        className={mc(
+          "w-28 h-28 p-3 rounded-lg bg-background-8 grid place-items-center",
+          small && "p-2 lg:w-[72px] lg:h-[72px]"
+        )}
+      >
+        <div
+          className={mc(
+            "relative w-[88px] h-[88px]",
+            small && small && "lg:w-[56px] lg:h-[56px]"
+          )}
+        >
+          <Image
+            alt={(image.alt as string) || `${name}`}
+            src={imageUrl}
+            fill
+            className="rounded-xl object-contain"
+          />
+        </div>
+      </div>
+    </Tooltip>
+  );
+};
 
 const SkillsContainer = ({
   className,
@@ -35,6 +78,9 @@ const SkillsContainer = ({
 );
 
 async function Skills() {
+  const techSkills: TSkill[] = await getTechSkills();
+
+  const skillsByGroup = groupBy(techSkills, "group");
   return (
     <Container className="px-3 py-6 lg:py-10 grid gap-16 md:gap-24">
       <section className="grid gap-6">
@@ -52,42 +98,43 @@ async function Skills() {
         <div className="grid lg:grid-cols-12 gap-4">
           <SkillsContainer className="lg:col-start-1 lg:col-end-9 lg:row-start-1 lg:row-end-3">
             <Text variant="subheading">Main stack</Text>
-            <div className="flex flex-wrap gap-3 w-full justify-center lg:justify-between">
-              <TempBox></TempBox>
-              <TempBox></TempBox>
-              <TempBox></TempBox>
-              <TempBox></TempBox>
+            <div className="flex flex-wrap gap-3 w-full justify-center lg:justify-around">
+              {skillsByGroup["main-stack"].map((skill) => (
+                <SkillsImage key={skill._id} skill={skill} />
+              ))}
             </div>
           </SkillsContainer>
           <SkillsContainer className="lg:col-start-9 lg:col-end-13 lg:row-start-1 lg:row-end-4">
             <Text variant="body2">Fundations</Text>
             <div className="flex flex-wrap justify-center w-full gap-3">
-              <TempBox></TempBox>
-              <TempBox></TempBox>
-              <TempBox></TempBox>
+              {skillsByGroup["basics"].map((skill) => (
+                <SkillsImage key={skill._id} skill={skill} />
+              ))}
             </div>
           </SkillsContainer>
           <SkillsContainer className="justify-center lg:col-start-1 lg:col-end-6 lg:row-start-3 lg:row-end-5">
             <Text variant="body2">Styling</Text>
             <div className="flex xs:flex-wrap justify-center w-full gap-3">
-              <TempBox></TempBox>
-              <TempBox></TempBox>
-              <TempBox></TempBox>
+              {skillsByGroup["system-design"].map((skill) => (
+                <SkillsImage key={skill._id} skill={skill} />
+              ))}
             </div>
           </SkillsContainer>
 
           <SkillsContainer className="lg:col-start-6 lg:col-end-9 lg:row-start-3 lg:row-end-6">
             <Text variant="body2">Animation</Text>
             <div className="flex w-full lg:flex-col gap-3 justify-center items-center">
-              <TempBox></TempBox>
-              <TempBox></TempBox>
+              {skillsByGroup["animations"].map((skill) => (
+                <SkillsImage key={skill._id} skill={skill} />
+              ))}
             </div>
           </SkillsContainer>
           <SkillsContainer className="lg:col-start-9 lg:col-end-13 lg:row-start-4 lg:row-end-6">
             <Text variant="body2">Control Version</Text>
             <div className="flex w-full gap-3 justify-center lg:justify-center">
-              <TempBox></TempBox>
-              <TempBox></TempBox>
+              {skillsByGroup["version-control"].map((skill) => (
+                <SkillsImage key={skill._id} skill={skill} />
+              ))}
             </div>
           </SkillsContainer>
           <SkillsContainer className="flex items-end lg:col-start-1 lg:col-end-6 lg:row-start-5 lg:lg:row-end-6">
@@ -95,19 +142,22 @@ async function Skills() {
               Mas herramientas
             </Text>
             <span className="inline-block">
-              <ArrowDownIcon className="w-12 h-12 text-primary-9 rotate-12 lg:-rotate-12" />
+              <ArrowDownIcon className="w-12 h-12 text-primary-9" />
             </span>
           </SkillsContainer>
           <SkillsContainer className="items-center lg:col-start-1 lg:col-end-11 lg:row-start-6 lg:lg:row-end-7">
             <div className="w-full row-span-2 flex flex-wrap items-center lg:justify-around gap-3">
+              {skillsByGroup["backend"].map((skill) => (
+                <SkillsImage small key={skill._id} skill={skill} />
+              ))}
+              {skillsByGroup["others"].map((skill) => (
+                <SkillsImage small key={skill._id} skill={skill} />
+              ))}
+              {/* <TempBox small></TempBox>
               <TempBox small></TempBox>
               <TempBox small></TempBox>
               <TempBox small></TempBox>
-              <TempBox small></TempBox>
-              <TempBox small></TempBox>
-              <TempBox small></TempBox>
-              <TempBox small></TempBox>
-              <TempBox small></TempBox>
+              <TempBox small></TempBox> */}
             </div>
           </SkillsContainer>
           <Card className="p-3 grid items-center justify-center lg:col-start-11 lg:lg:col-end-13 lg:row-start-6 lg:lg:row-end-7">
