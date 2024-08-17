0af7142e11ef46ceee96d7068e68916cd7222aa1
jeantivan <jptivan53@gmail.com>
2024-08-17 20:35:38 +0200

    CHANGE: Cambio del tamaño de fuente en los textos. ADD: Uso de i18n

--- src/app/(site)/[lng]/contact/page.tsx
+++ src/app/(site)/[lng]/contact/page.tsx
@@ -2,42 +2,56 @@ import Heading from "@/components/Heading";
 
 import Container from "@/components/common/Container";
 import Text from "@/components/common/Text";
+import { useTranslation } from "@/src/app/i18n";
 
 export const metadata = {
   title: "Contact",
   description: "Let's connect and work together!"
 };
 
-function Contact() {
+type PageProps = {
+  params: {
+    lng: string;
+  };
+};
+async function Contact({ params: { lng } }: PageProps) {
+  const { t } = await useTranslation(lng, "contact");
+
   return (
     <Container className="my-8 md:my-16">
       <section className="">
         <Heading
-          text="Let's connect & work together"
+          text={t("title")}
           Component="h1"
           variant="title"
+          display
           className="uppercase mb-4 text-center"
         />
 
-        <Text variant="subtitle" color="secondary" className="mb-6 text-center">
-          Lorem ipsum dolor sit amet consectetur. Pharetra a in aliquet
-          malesuada. Id interdum cursus mi in.
+        <Text variant="body2" color="secondary" className="mb-6 text-center">
+          {t("subtitle")}
         </Text>
         <Heading
           text="jptivan52@gmail.com"
-          variant="heading"
+          variant="title"
           className="mb-8 text-center"
         />
-        <Text variant="body1" color="secondary" className="text-center">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo
-          reprehenderit{" "}
-          <a className="text-primary-9 hover:underline" href="">
-            Linkedin
+        <Text variant="body2" color="secondary" className="text-center">
+          {t("social-cta.description")}{" "}
+          <a
+            className="text-primary-9 hover:underline"
+            href="https://www.linkedin.com/"
+          >
+            LinkedIn
           </a>{" "}
-          or{" "}
-          <a className="text-primary-9 hover:underline" href="">
-            Upwork
+          {t("social-cta.or")}{" "}
+          <a
+            className="text-primary-9 hover:underline"
+            href="https://github.com/"
+          >
+            Github
           </a>
+          {t("social-cta.end")}
         </Text>
       </section>
     </Container>
