28a9280421dcb5a8c84804543f2eb23271b67180
jeantivan <jptivan53@gmail.com>
2024-08-19 15:29:59 +0200

    ADD: i18n para el contenido del componente

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,21 +1,18 @@
 import { ReactNode, useState } from "react";
-import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import {
   Cross1Icon,
   DownloadIcon,
   GitHubLogoIcon,
   HamburgerMenuIcon,
-  LinkedInLogoIcon,
-  MoonIcon
+  LinkedInLogoIcon
 } from "@radix-ui/react-icons";
 
-import IconButton from "./common/IconButton";
+import { mc } from "@/utils/helpers";
+import { useClientTranslation } from "@/src/app/i18n/client";
 
-import { useIsExpanded } from "./Header";
+import IconButton from "./common/IconButton";
 import Button from "./common/Button";
-import { Tooltip } from "./common/Tooltip";
-import { mc } from "../utils/helpers";
 import DarkMode from "./DarkMode";
 import Logo from "./Logo";
 
@@ -61,43 +58,58 @@ function Nav({
   closeNav: () => void;
 }) {
   const pathname = usePathname();
+  const { t } = useClientTranslation(lng, "common");
 
   return (
     <div className="fixed inset-0 z-50">
       <div className="bg-background-2 p-3 w-full h-full grid grid-rows-[min-content_1fr_min-content]">
         <header className="flex gap-6 items-center justify-between py-2">
-          <IconButton onClick={toggle} label="Cerrar nav" icon={Cross1Icon} />
-          <DarkMode />
+          <IconButton
+            onClick={toggle}
+            label={t("navbar.close")}
+            icon={Cross1Icon}
+          />
+          <DarkMode lng={lng} />
         </header>
         <nav className="flex flex-col justify-center items-center w-full gap-8">
-          <NavLink href="/" isActive={pathname === "/"} closeNav={closeNav}>
-            Sobre mi
-          </NavLink>
           <NavLink
-            href="/projects"
-            isActive={pathname === "/projects"}
+            href={t(`/${lng}`)}
+            isActive={pathname === `/${lng}`}
             closeNav={closeNav}
           >
-            Proyectos
+            {t("navbar.links.about-me")}
           </NavLink>
           <NavLink
-            href="/skills"
-            isActive={pathname === "/skills"}
+            href={t(`/${lng}/projects`)}
+            isActive={pathname === `/${lng}/projects`}
             closeNav={closeNav}
           >
-            Habilidades
+            {t("navbar.links.projects")}
           </NavLink>
           <NavLink
-            href="/contact"
-            isActive={pathname === "/contact"}
+            href={t(`/${lng}/skills`)}
+            isActive={pathname === `/${lng}/skills`}
             closeNav={closeNav}
           >
-            Contacto
+            {t("navbar.links.skills")}
+          </NavLink>
+          <NavLink
+            href={t(`/${lng}/contact`)}
+            isActive={pathname === `/${lng}/contact`}
+            closeNav={closeNav}
+          >
+            {t("navbar.links.contact")}
           </NavLink>
         </nav>
         <footer className="flex gap-6 justify-center py-3">
-          <IconButton icon={LinkedInLogoIcon} label="LinkedIn Profile" />
-          <IconButton icon={GitHubLogoIcon} label="LinkedIn Profile" />
+          <IconButton
+            icon={LinkedInLogoIcon}
+            label={t("navbar.mobile.linkedin-btn")}
+          />
+          <IconButton
+            icon={GitHubLogoIcon}
+            label={t("navbar.mobile.github-btn")}
+          />
         </footer>
       </div>
     </div>
@@ -105,6 +117,8 @@ function Nav({
 }
 
 function MobileNav({ lng }: { lng: string }) {
+  const { t } = useClientTranslation(lng, "common");
+
   const [showNav, setShowNav] = useState<boolean>(false);
 
   const toggle = () => setShowNav(!showNav);
@@ -116,13 +130,13 @@ function MobileNav({ lng }: { lng: string }) {
       <div className="flex gap-6 items-center py-2">
         <IconButton
           onClick={toggle}
-          label="Abrir navbar"
+          label={t("navbar.mobile.open")}
           color="secondary"
           icon={HamburgerMenuIcon}
         />
         <Logo lng={lng} mobile />
         <Button as="a">
-          Descargar CV{" "}
+          {t("navbar.cv")}{" "}
           <span className="inline-flex">
             <DownloadIcon />
           </span>
