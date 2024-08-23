e12d50a7c696bc1aee99cdcaffb517d99d118b4f
jeantivan <jptivan53@gmail.com>
2024-08-23 19:39:05 +0200

    ADD: Nuevos esquemas

--- src/sanity/schema/index.ts
+++ src/sanity/schema/index.ts
@@ -3,9 +3,17 @@ import { SchemaTypeDefinition } from "sanity";
 import { techSkillsSchema } from "./tech-skills-schema";
 import { projectsSchema } from "./projects-schema";
 import { imageWithCaptionSchema } from "./image-with-caption-schema";
+import { pagesSchema } from "./pages-schema";
+import { settingsSchema } from "./settings-schema";
 
 const schema: { types: SchemaTypeDefinition[] } = {
-  types: [projectsSchema, techSkillsSchema, imageWithCaptionSchema],
+  types: [
+    projectsSchema,
+    techSkillsSchema,
+    imageWithCaptionSchema,
+    pagesSchema,
+    settingsSchema
+  ]
 };
 
 export default schema;
