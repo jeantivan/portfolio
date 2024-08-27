6edbf50ea187d43ecff3946b22f678743761463b
Jean <jptivan53@gmail.com>
2024-08-27 22:44:17 +0200

    CHANGE:  Cambio en la organizacion de los archivos que componen al esquema de sanity

--- src/sanity/schema/index.ts
+++ src/sanity/schema/index.ts
@@ -1,18 +1,30 @@
-import { SchemaTypeDefinition } from "sanity";
+import type { SchemaTypeDefinition } from "sanity";
 
-import { techSkillsSchema } from "./tech-skills-schema";
-import { projectsSchema } from "./projects-schema";
-import { imageWithCaptionSchema } from "./image-with-caption-schema";
-import { pagesSchema } from "./pages-schema";
-import { settingsSchema } from "./settings-schema";
+import { techSkillsSchema } from "./documents/tech-skills";
+import { projectsSchema } from "./documents/projects";
+import { pagesSchema } from "./documents/pages";
+import { settingsSchema } from "./documents/settings";
+import { skillGroupsSchema } from "./documents/skill-groups";
+
+import { localizedSlug } from "./objects/localized-slug";
+import { localizedString } from "./objects/localized-string";
+import { imageWithCaption } from "./objects/image-with-caption";
+import { portableText } from "./objects/portable-text";
 
 const schema: { types: SchemaTypeDefinition[] } = {
   types: [
+    // Documents
     projectsSchema,
     techSkillsSchema,
-    imageWithCaptionSchema,
     pagesSchema,
-    settingsSchema
+    settingsSchema,
+    skillGroupsSchema,
+
+    // Objects
+    imageWithCaption,
+    localizedSlug,
+    localizedString,
+    portableText
   ]
 };
 
