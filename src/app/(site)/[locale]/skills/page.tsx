b57be5df035896b9628a20085a7336f1a9b67ada
Jean <jptivan53@gmail.com>
2024-09-12 22:58:33 +0200

    CHANGE: Nuevo maquetado, diseño y contenido para la página Skills

--- src/app/(site)/[locale]/skills/page.tsx
+++ src/app/(site)/[locale]/skills/page.tsx
@@ -1,65 +1,16 @@
-import { ReactNode } from "react";
-import { groupBy } from "lodash";
+import "@/styles/skills-page.css";
+
+import { ReactNode, Fragment } from "react";
 import Card from "@/src/components/common/Card";
 import Container from "@/src/components/common/Container";
 import Text from "@/src/components/common/Text";
 import Heading from "@/src/components/Heading";
 import { mc } from "@/src/utils/helpers";
 import { ArrowDownIcon } from "@radix-ui/react-icons";
-import { TSkill } from "@/src/utils/types";
-import { getTechSkills } from "@/src/sanity/queries";
-import { urlForImage } from "@/src/sanity/utils";
-import Image from "next/image";
-import { Tooltip } from "@/src/components/common/Tooltip";
+import { getSkillsGroups } from "@/src/sanity/queries";
 import { getTranslations } from "next-intl/server";
-
-// Tecnologias favoritas que uso para construir experiencias
-
-const TempBox = ({ small = false }: { small?: boolean }) => (
-  <div
-    className={mc(
-      "w-28 h-28 rounded-lg bg-background-6",
-      small && "lg:w-[72px] lg:h-[72px]"
-    )}
-  ></div>
-);
-
-const SkillsImage = ({
-  small = false,
-  skill
-}: {
-  small?: boolean;
-  skill: TSkill;
-}) => {
-  const { name, image } = skill;
-
-  const imageUrl = urlForImage(image)?.width(176).url() ?? "";
-
-  return (
-    <Tooltip content={name}>
-      <div
-        className={mc(
-          "w-28 h-28 p-3 rounded-lg bg-background-3/50 dark:bg-background-8/50 backdrop-blur-sm grid place-items-center",
-          small && "p-2 lg:w-[72px] lg:h-[72px]"
-        )}
-      >
-        <div
-          className={mc(
-            "relative w-[88px] h-[88px]",
-            small && small && "lg:w-[56px] lg:h-[56px]"
-          )}
-        >
-          <Image
-            alt={(image.alt as string) || `${name}`}
-            src={imageUrl}
-            fill
-            className="rounded-xl object-contain"
-          />
-        </div>
-      </div>
-    </Tooltip>
-  );
-};
+import Skill from "@/src/components/Skill";
+import { TSkill } from "@/src/utils/types";
 
 const SkillsContainer = ({
   className,
@@ -70,24 +21,63 @@ const SkillsContainer = ({
 }) => (
   <Card
     className={mc(
-      "py-6 px-3 lg:px-6 grid grid-rows-[min-content_min-content] gap-6",
+      "p-5 grid grid-rows-[min-content_min-content] gap-5",
       className
     )}
   >
     {children}
   </Card>
 );
+const ReactEcosystemTechs = ({ techs }: { techs: TSkill[] }) => {
+  const showSuspense = techs.length >= 8;
+
+  const techsToShow = techs.slice(0, 7);
+
+  return (
+    <div className="flex w-full flex-wrap gap-2 justify-start items-center">
+      {techsToShow.map((skill) => (
+        <Skill size="small" key={skill._id} skill={skill} />
+      ))}
+      {showSuspense && (
+        <div
+          className={mc(
+            "aspect-square p-2 rounded-lg grid place-items-center",
+            "w-[66px]"
+          )}
+        >
+          <Text
+            display
+            color="primary"
+            variant="heading"
+            className="-mt-5 leading-none tracking-wider"
+          >
+            ...
+          </Text>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export const metadata = {
   title: "Contact",
   description: "Let's connect and work together!"
 };
 
-async function Skills() {
+type PageProps = {
+  params: {
+    locale: string;
+  };
+};
+async function Skills({ params: { locale } }: PageProps) {
   const t = await getTranslations("SkillsPage");
-  const techSkills: TSkill[] = await getTechSkills();
 
-  const skillsByGroup = groupBy(techSkills, "group");
+  const skillsGroups = await getSkillsGroups();
+
+  const inTheBackendGroup = skillsGroups.find(
+    (skillGroup) => skillGroup.slug.en.current === "in-the-backend"
+  );
+
   return (
     <Container className="px-3 py-6 lg:py-10 grid gap-16 md:gap-20">
       <section className="grid gap-6">
@@ -95,74 +85,140 @@ async function Skills() {
           <Heading
             text={t("main.title")}
             variant="title"
-            className="text-center mb-4"
+            className="mb-4"
             display
           />
           <Text variant="body3" color="secondary" className="">
             {t("main.content")}
           </Text>
         </div>
-        <div className="grid lg:grid-cols-12 gap-4">
-          <SkillsContainer className="lg:col-start-1 lg:col-end-9 lg:row-start-1 lg:row-end-3">
+        <div className="bento gap-4">
+          {skillsGroups.map(({ _id, title, slug, techs }) => {
+            const label = title[locale as "en" | "es"];
+
+            switch (slug.en.current) {
+              case "in-the-backend":
+                return null;
+              case "more-technologies":
+                return (
+                  <Fragment key={_id}>
+                    <Card
+                      case
+                      className={mc("helper", "p-5 flex items-center gap-6")}
+                    >
+                      <ArrowDownIcon className="text-primary-9 w-8 h-8"></ArrowDownIcon>
+                      <Text className="lg:text-xl leading-none">{label}</Text>
+                    </Card>
+                    <SkillsContainer
+                      className={mc(
+                        slug.en.current,
+                        "place-items-center grid-rows-auto"
+                      )}
+                    >
+                      <div className="flex flex-wrap gap-4">
+                        {inTheBackendGroup &&
+                          inTheBackendGroup.techs?.map((tech) => (
+                            <Skill size="small" key={_id} skill={tech} />
+                          ))}
+                        {techs?.map((tech) => (
+                          <Skill size="small" key={_id} skill={tech} />
+                        ))}
+                      </div>
+                    </SkillsContainer>
+                  </Fragment>
+                );
+              case "the-main-stack":
+                return (
+                  <SkillsContainer
+                    key={_id}
+                    className={mc("lg:pt-12", slug.en.current)}
+                  >
+                    <Text display variant="subheading">
+                      {label}
+                    </Text>
+                    <div className="w-full flex flex-wrap justify-evenly md:justify-start gap-2 md:gap-4">
+                      {techs?.map((tech) => (
+                        <Skill size="large" key={_id} skill={tech} />
+                      ))}
+                    </div>
+                  </SkillsContainer>
+                );
+              case "version-control":
+                return (
+                  <SkillsContainer key={_id} className={`${slug.en.current}`}>
+                    <Text variant="body2">{label}</Text>
+                    <div className="flex w-full flex-col md:flex-row gap-2 md:gap-4 justify-center items-center">
+                      {techs?.map((skill) => (
+                        <Skill key={skill._id} skill={skill} />
+                      ))}
+                    </div>
+                  </SkillsContainer>
+                );
+              case "animations":
+                return (
+                  <SkillsContainer key={_id} className={`${slug.en.current}`}>
+                    <Text variant="body2">{label}</Text>
+                    <div className="flex w-full flex-col md:flex-row lg:flex-col gap-2 md:gap-4 justify-center items-center">
+                      {techs?.map((skill) => (
+                        <Skill key={skill._id} skill={skill} />
+                      ))}
+                    </div>
+                  </SkillsContainer>
+                );
+              case "react-ecosystem":
+                const reTechs = [
+                  ...techs,
+                  ...techs,
+                  ...techs,
+                  ...techs,
+                  ...techs,
+                  ...techs,
+                  ...techs,
+                  ...techs,
+                  ...techs,
+                  ...techs,
+                  ...techs,
+                  ...techs,
+                  ...techs
+                ];
+                return (
+                  <SkillsContainer key={_id} className={`${slug.en.current}`}>
+                    <Text variant="body2">{label}</Text>
+                    <ReactEcosystemTechs techs={reTechs} />
+                  </SkillsContainer>
+                );
+
+              default:
+                return (
+                  <SkillsContainer
+                    key={_id}
+                    className={mc(
+                      slug.en.current,
+                      "flex flex-col justify-between"
+                    )}
+                  >
+                    <Text variant="body2">{label}</Text>
+                    <div className="flex justify-evenly gap-2 md:gap-4">
+                      {techs?.map((tech) => (
+                        <Skill size="big" key={_id} skill={tech} />
+                      ))}
+                    </div>
+                  </SkillsContainer>
+                );
+            }
+
+            return null;
+          })}
+          {/* <SkillsContainer className="lg:col-start-1 lg:col-end-9 lg:row-start-1 lg:row-end-3">
             <Text variant="body2">{t("skills.main-stack")}</Text>
             <div className="flex flex-wrap gap-3 w-full justify-center lg:justify-around">
-              {skillsByGroup["main-stack"].map((skill) => (
+               grouped["las-tecnologias-principales"][0].techs?.map((skill) => (
                 <SkillsImage key={skill._id} skill={skill} />
-              ))}
+              )) 
             </div>
-          </SkillsContainer>
-          <SkillsContainer className="lg:col-start-9 lg:col-end-13 lg:row-start-1 lg:row-end-4">
-            <Text variant="body2">{t("skills.basics")}</Text>
-            <div className="flex flex-wrap justify-center w-full gap-3">
-              {skillsByGroup["basics"].map((skill) => (
-                <SkillsImage key={skill._id} skill={skill} />
-              ))}
-            </div>
-          </SkillsContainer>
-          <SkillsContainer className="justify-center lg:col-start-1 lg:col-end-6 lg:row-start-3 lg:row-end-5">
-            <Text variant="body2">{t("skills.styling")}</Text>
-            <div className="flex xs:flex-wrap justify-center w-full gap-3">
-              {skillsByGroup["system-design"].map((skill) => (
-                <SkillsImage key={skill._id} skill={skill} />
-              ))}
-            </div>
-          </SkillsContainer>
+          </SkillsContainer> */}
 
-          <SkillsContainer className="lg:col-start-6 lg:col-end-9 lg:row-start-3 lg:row-end-6">
-            <Text variant="body2">{t("skills.animations")}</Text>
-            <div className="flex w-full lg:flex-col gap-3 justify-center items-center">
-              {skillsByGroup["animations"].map((skill) => (
-                <SkillsImage key={skill._id} skill={skill} />
-              ))}
-            </div>
-          </SkillsContainer>
-          <SkillsContainer className="lg:col-start-9 lg:col-end-13 lg:row-start-4 lg:row-end-6">
-            <Text variant="body2">{t("skills.version-control")}</Text>
-            <div className="flex w-full gap-3 justify-center lg:justify-center">
-              {skillsByGroup["version-control"].map((skill) => (
-                <SkillsImage key={skill._id} skill={skill} />
-              ))}
-            </div>
-          </SkillsContainer>
-          <SkillsContainer className="flex items-end lg:col-start-1 lg:col-end-6 lg:row-start-5 lg:lg:row-end-6">
-            <Text className="flex-1" variant="subheading" display>
-              {t("skills.others")}
-            </Text>
-            <span className="inline-block">
-              <ArrowDownIcon className="w-12 h-12 text-primary-9" />
-            </span>
-          </SkillsContainer>
-          <SkillsContainer className="items-center lg:col-start-1 lg:col-end-11 lg:row-start-6 lg:lg:row-end-7">
-            <div className="w-full row-span-2 flex flex-wrap items-center lg:justify-around gap-3">
-              {skillsByGroup["backend"].map((skill) => (
-                <SkillsImage small key={skill._id} skill={skill} />
-              ))}
-              {skillsByGroup["others"].map((skill) => (
-                <SkillsImage small key={skill._id} skill={skill} />
-              ))}
-            </div>
-          </SkillsContainer>
-          <Card className="p-3 grid items-center justify-center lg:col-start-11 lg:lg:col-end-13 lg:row-start-6 lg:lg:row-end-7">
+          <Card className="hidden p-3 lg:grid items-center justify-center suspense">
             <Text
               className="tracking-wide -mt-4"
               variant="heading"
