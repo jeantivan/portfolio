9bbffefab8a94912f680a66e3a55e94db4dc39e0
Jean <jptivan53@gmail.com>
2024-09-18 00:39:05 +0200

    ADD: Types para las props de los componentes del DropdownMenu (estos types son copiados de su definición por @radix-ui)

--- src/components/common/DropdownMenu.tsx
+++ src/components/common/DropdownMenu.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { ComponentPropsWithoutRef } from "react";
 import cx from "classnames";
 import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
 
@@ -10,95 +11,104 @@ const DropdownMenuRadioGroup = DropdownMenuPrimitive.RadioGroup;
 const DropdownMenuGroup = DropdownMenuPrimitive.Group;
 
 const itemClassName = cx(
-  "px-3 flex items-center gap-2 select-none cursor-pointer outline-none",
-  "hover:bg-gold-4 data-[disabled]:bg-gold-3 data-[disabled]:text-gold-8 data-[disabled]:cursor-auto"
+	"px-3 flex items-center gap-2 select-none cursor-pointer outline-none",
+	"hover:bg-gold-4 data-[disabled]:bg-gold-3 data-[disabled]:text-gold-8 data-[disabled]:cursor-auto"
 );
 
-const DropdownMenuItem = ({
-  className,
-  ...rest
-}: DropdownMenuPrimitive.MenuItemProps) => (
-  <DropdownMenuPrimitive.Item
-    className={cx(itemClassName, className)}
-    {...rest}
-  >
-    {rest.children}
-  </DropdownMenuPrimitive.Item>
+type DropdownMenuItemProps = ComponentPropsWithoutRef<
+	typeof DropdownMenuPrimitive.Item
+>;
+const DropdownMenuItem = ({ className, ...rest }: DropdownMenuItemProps) => (
+	<DropdownMenuPrimitive.Item
+		className={cx(itemClassName, className)}
+		{...rest}
+	>
+		{rest.children}
+	</DropdownMenuPrimitive.Item>
 );
 
+type DropdownMenuCheckboxItemProps = ComponentPropsWithoutRef<
+	typeof DropdownMenuPrimitive.DropdownMenuCheckboxItem
+>;
 const DropdownMenuCheckboxItem = ({
-  className,
-  children,
-  ...rest
-}: DropdownMenuPrimitive.DropdownMenuCheckboxItemProps) => (
-  <DropdownMenuPrimitive.DropdownMenuCheckboxItem
-    className={cx(
-      itemClassName,
-      "data-[state=checked]:bg-primary-4",
-      "data-[state=checked]:text-primary-9",
-      "relative",
-      className
-    )}
-    {...rest}
-  >
-    {children}
-    <DropdownMenuPrimitive.ItemIndicator className="absolute w-7 right-0 inline-flex items-center justify-center">
-      <CheckIcon className="fill-current" />
-    </DropdownMenuPrimitive.ItemIndicator>
-  </DropdownMenuPrimitive.DropdownMenuCheckboxItem>
+	className,
+	children,
+	...rest
+}: DropdownMenuCheckboxItemProps) => (
+	<DropdownMenuPrimitive.DropdownMenuCheckboxItem
+		className={cx(
+			itemClassName,
+			"data-[state=checked]:bg-primary-4",
+			"data-[state=checked]:text-primary-9",
+			"relative",
+			className
+		)}
+		{...rest}
+	>
+		{children}
+		<DropdownMenuPrimitive.ItemIndicator className="absolute w-7 right-0 inline-flex items-center justify-center">
+			<CheckIcon className="fill-current" />
+		</DropdownMenuPrimitive.ItemIndicator>
+	</DropdownMenuPrimitive.DropdownMenuCheckboxItem>
 );
 
+type DropdownMenuRadioItemProps = ComponentPropsWithoutRef<
+	typeof DropdownMenuPrimitive.DropdownMenuRadioItem
+>;
 const DropdownMenuRadioItem = ({
-  children,
-  className,
-  ...rest
-}: DropdownMenuPrimitive.DropdownMenuRadioItemProps) => (
-  <DropdownMenuPrimitive.DropdownMenuRadioItem
-    className={cx(
-      itemClassName,
-      "data-[state=checked]:bg-primary-4",
-      "data-[state=checked]:text-primary-9",
-      "relative",
-      className
-    )}
-    {...rest}
-  >
-    {children}
-    <DropdownMenuPrimitive.ItemIndicator className="absolute w-7 right-0 inline-flex items-center justify-center">
-      <CheckIcon className="fill-current" />
-    </DropdownMenuPrimitive.ItemIndicator>
-  </DropdownMenuPrimitive.DropdownMenuRadioItem>
+	children,
+	className,
+	...rest
+}: DropdownMenuRadioItemProps) => (
+	<DropdownMenuPrimitive.DropdownMenuRadioItem
+		className={cx(
+			itemClassName,
+			"data-[state=checked]:bg-primary-4",
+			"data-[state=checked]:text-primary-9",
+			"relative",
+			className
+		)}
+		{...rest}
+	>
+		{children}
+		<DropdownMenuPrimitive.ItemIndicator className="absolute w-7 right-0 inline-flex items-center justify-center">
+			<CheckIcon className="fill-current" />
+		</DropdownMenuPrimitive.ItemIndicator>
+	</DropdownMenuPrimitive.DropdownMenuRadioItem>
 );
 
+type DropdownMenuContentProps = ComponentPropsWithoutRef<
+	typeof DropdownMenuPrimitive.Content
+>;
 const DropdownMenuContent = ({
-  className,
-  sideOffset = 3,
-  ...rest
-}: DropdownMenuPrimitive.DropdownMenuContentProps) => (
-  <DropdownMenuPrimitive.Portal>
-    <DropdownMenuPrimitive.Content
-      sideOffset={sideOffset}
-      {...rest}
-      className={cx(
-        "bg-gold-2 text-gold-12 text-lg z-50 min-w-[9rem] overflow-hidden",
-        "border border-gold-4 rounded-md",
-        "py-1.5",
-        className
-      )}
-      id="DropdownMenu"
-    >
-      {rest.children}
-    </DropdownMenuPrimitive.Content>
-  </DropdownMenuPrimitive.Portal>
+	className,
+	sideOffset = 3,
+	...rest
+}: DropdownMenuContentProps) => (
+	<DropdownMenuPrimitive.Portal>
+		<DropdownMenuPrimitive.Content
+			sideOffset={sideOffset}
+			{...rest}
+			className={cx(
+				"bg-gold-2 text-gold-12 text-lg z-50 min-w-[9rem] overflow-hidden",
+				"border border-gold-4 rounded-md",
+				"py-1.5",
+				className
+			)}
+			id="DropdownMenu"
+		>
+			{rest.children}
+		</DropdownMenuPrimitive.Content>
+	</DropdownMenuPrimitive.Portal>
 );
 
 export {
-  DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuGroup,
-  DropdownMenuRadioGroup,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuCheckboxItem,
-  DropdownMenuRadioItem,
+	DropdownMenu,
+	DropdownMenuTrigger,
+	DropdownMenuGroup,
+	DropdownMenuRadioGroup,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuCheckboxItem,
+	DropdownMenuRadioItem
 };
