29a59820ecadd593593c81ec21e10bb758ce8127
Jean <jptivan53@gmail.com>
2024-10-16 23:40:43 +0200

    CHANGE: Descartado el uso de Sanity se usará astro:content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import sanity from '@sanity/astro';
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-
-  integrations: [sanity({
-    projectId: "ezr5hgw5",
-    dataset: "production",
-    useCdn: false,
-    studioBasePath: "/admin",
-  }), react(), tailwind()]
+  integrations: [react(), tailwind()]
 });
