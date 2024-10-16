32fdd8a28c79fd53b05cd6dbdb37621ab7b96b69
Jean <jptivan53@gmail.com>
2024-10-17 00:15:53 +0200

    ADD: Configurando y montando paraglide para i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,25 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
-
+import paraglide from "@inlang/paraglide-astro";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  i18n: {
+    locales: ["es", "en"],
+    defaultLocale: "es",
+    routing: {
+      prefixDefaultLocale: true
+    }
+  },
+
+  integrations: [
+    react(),
+    tailwind(),
+    paraglide({
+      project: "./project.inlang",
+      outdir: "./src/paraglide"
+    })
+  ]
 });
