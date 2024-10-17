46a3b86c9b186144d407f744d268e701829b63b2
Jean <jptivan53@gmail.com>
2024-10-17 21:09:17 +0200

    DELETE: Types necesarios para sanity

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,13 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-/// <reference types="@sanity/astro/module" />
-
-interface ImportMetaEnv {
-  readonly SANITY_PROJECT_ID: string;
-  readonly SANITY_DATASET: string;
-  readonly SANITY_API_VERSION: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
