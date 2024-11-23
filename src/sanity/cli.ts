25cce993530e13382e9bc1974d2ee0e351524f24
Jean <jptivan53@gmail.com>
2024-11-24 00:14:48 +0100

    DELETE: Archivos relacionados con sanity

--- src/sanity/cli.ts
+++
@@ -1,8 +0,0 @@
-// ./sanity.cli.ts
-/* eslint-disable no-process-env */
-import { defineCliConfig } from "sanity/cli";
-
-const projectId = import.meta.env.SANITY_PROJECT_ID;
-const dataset = import.meta.env.SANITY_DATASET;
-
-export default defineCliConfig({ api: { projectId, dataset } });
--- src/sanity/schema/objects/portable-text.ts
+++
@@ -1,7 +0,0 @@
-import { defineType, defineField } from "sanity";
-
-export const portableText = defineType({
-  name: "portableText",
-  type: "array",
-  of: [{ type: "block" }]
-});
