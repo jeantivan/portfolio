bc18e252b9e5a0267aec507575930b0e5f333a56
Jean <jptivan53@gmail.com>
2024-11-25 17:19:35 +0100

    DELETE: Todo lo relacionado con next.js

--- old/src/utils/constants.ts
+++
@@ -1,3 +0,0 @@
-export const PRIMARY_COLOR_KEY = "jeantivan-portfolio-primary-color";
-export const COLOR_MODE_KEY = "jeantivan-portfolio-color-mode";
-export const COLORS = ["orange", "indigo", "grass", "amber", "purple"] as const;
